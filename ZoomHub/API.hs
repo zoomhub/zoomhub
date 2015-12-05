ce7b2deeafd154c78206181940a9c008c4820f03
Daniel Gasienica <daniel@gasienica.ch>
2015-12-04 23:49:13 -0800

    Use more qualified imports

--- ZoomHub/API.hs
+++ ZoomHub/API.hs
@@ -4,25 +4,27 @@
 module ZoomHub.API where
 
 import Control.Applicative
-import Control.Monad.Trans.Either
 import Data.Aeson as Aeson
 import Data.Char
 import GHC.Generics
 import Network.Wai
-import Servant
+import Servant as S
 import System.Directory
 import ZoomHub.Types.Content
 
 import qualified Control.Monad.IO.Class as IO
+import qualified Control.Monad.Trans.Either as Either
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Proxy as Proxy
 
 
 -- Servant default handler type
-type Handler a = EitherT ServantErr IO a
+type Handler a = Either.EitherT S.ServantErr IO a
 
 -- API
-type API = "v1" :> "content" :> Capture "id" String :> Get '[JSON] Content
-      :<|> "v1" :> "content" :> QueryParam "url" String :> Get '[JSON] Content
+type API =
+      "v1" :> "content" :> S.Capture "id" String :> S.Get '[S.JSON] Content
+ :<|> "v2" :> "content" :> S.QueryParam "url" String :> S.Get '[S.JSON] Content
 
 -- Handlers
 getContentFromFile :: String -> IO (Maybe Content)
@@ -34,19 +36,19 @@ contentById :: String -> Handler Content
 contentById id = do
   maybeContent <- IO.liftIO $ getContentFromFile id
   case maybeContent of
-    Nothing -> left Servant.err404{errBody="ID not found"}
+    Nothing -> Either.left S.err404{errBody="ID not found"}
     Just c  -> return c
 
 contentByURL :: Maybe String -> Handler Content
 contentByURL url = case url of
-  Nothing  -> left Servant.err400{
+  Nothing  -> Either.left S.err400{
     errBody="Please provide an ID or `url` query parameter."
   }
   Just url -> return . mkContent $ url
 
 -- API
-api :: Proxy API
-api = Proxy
+api :: Proxy.Proxy API
+api = Proxy.Proxy
 
 server :: Server API
 server = contentById
