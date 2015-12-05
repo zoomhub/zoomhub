1ed8b4e79622799d0f7ab5fdb4d53d84e265af14
Daniel Gasienica <daniel@gasienica.ch>
2015-12-05 12:55:30 -0800

    Implement Rackspace `getToken`

--- ZoomHub/Rackspace/CloudFiles.hs
+++ ZoomHub/Rackspace/CloudFiles.hs
@@ -2,6 +2,7 @@
 
 module ZoomHub.Rackspace.CloudFiles where
 
+import Control.Applicative as Applicative
 import Data.Aeson as Aeson
 
 import qualified Control.Monad.IO.Class as IO
@@ -23,20 +24,28 @@ instance Aeson.ToJSON Credentials where
         ]
       ]
 
+data Token = Token String deriving (Eq, Show)
+
+instance Aeson.FromJSON Token where
+    parseJSON (Object o) =
+      Token <$> ((o .: "access") >>= (.: "token") >>= (.: "id"))
+    parseJSON _ = empty
+
+
+-- API
 tokenURL :: String
 tokenURL = "https://identity.api.rackspacecloud.com/v2.0/tokens"
 
-getJSON :: String -> -- ^ username
-           String -> -- ^ API key
-           IO LBS.ByteString
-getJSON username apiKey = do
+getToken :: Credentials -> IO (Maybe Token)
+getToken credentials = do
   req <- IO.liftIO $ HTTP.parseUrl tokenURL
-  let credentials = Aeson.encode $ Credentials username apiKey
+  let credentialsBS = Aeson.encode $ credentials
   let req' = req {
               HTTP.method = "POST"
-            , HTTP.requestBody = HTTP.RequestBodyLBS credentials
+            , HTTP.requestBody = HTTP.RequestBodyLBS credentialsBS
             , HTTP.requestHeaders = [("Content-Type", "application/json")]
             }
   manager <- IO.liftIO $ HTTP.newManager HTTP.tlsManagerSettings
   res <- HTTP.httpLbs req' manager
-  return $ HTTP.responseBody res
+  let maybeToken = decode $ HTTP.responseBody res
+  return maybeToken
