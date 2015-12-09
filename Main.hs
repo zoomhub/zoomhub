07dff2fbcafec68c5bfd5b4dfbb5cf6b0e9b26b1
Daniel Gasienica <daniel@gasienica.ch>
2015-12-08 23:36:16 -0800

    Look up `PORT` from environment variable

--- Main.hs
+++ Main.hs
@@ -1,7 +1,11 @@
 import qualified Network.Wai.Handler.Warp as Warp
 
-
+import qualified System.Environment as System
 import qualified ZoomHub.API as ZoomHub
 
 main :: IO ()
-main = Warp.run 8000 ZoomHub.app
+main = do
+  maybePort <- System.lookupEnv "PORT"
+  case maybePort of
+    Nothing   -> fail "Please specify a `PORT`"
+    Just port -> Warp.run (read port) ZoomHub.app
