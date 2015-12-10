f8bac5edfc9b587fb69a091915ab8ef97c919a14
Daniel Gasienica <daniel@gasienica.ch>
2015-12-10 01:17:13 -0800

    Use qualified import for `ZoomHub.Types.Content`

--- ZoomHub/API.hs
+++ ZoomHub/API.hs
@@ -11,7 +11,6 @@ import Data.Char
 import Network.Wai
 import Servant as S
 import System.Directory
-import ZoomHub.Types.Content
 
 import qualified Control.Monad.IO.Class as IO
 import qualified Control.Monad.Trans.Either as Either
@@ -21,6 +20,7 @@ import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Lazy.Char8 as CL
 import qualified Data.Proxy as Proxy
 import qualified ZoomHub.Rackspace.CloudFiles as CF
+import qualified ZoomHub.Types.Content as ZH
 
 
 -- Servant default handler type
@@ -28,16 +28,17 @@ type Handler a = Either.EitherT S.ServantErr IO a
 
 -- API
 type API =
-      "v1" :> "content" :> S.Capture "id" String :> S.Get '[S.JSON] Content
- :<|> "v1" :> "content" :> S.QueryParam "url" String :> S.Get '[S.JSON] Content
+  "v1" :> "content" :> S.Capture "id" String :> S.Get '[S.JSON] ZH.Content
+  :<|>
+  "v1" :> "content" :> S.QueryParam "url" String :> S.Get '[S.JSON] ZH.Content
 
 -- Handlers
-getContentFromFile :: String -> IO (Maybe Content)
+getContentFromFile :: String -> IO (Maybe ZH.Content)
 getContentFromFile id = do
   cd <- getCurrentDirectory
   Aeson.decode <$> LBS.readFile (cd ++ "/data/content-by-id/" ++ id ++ ".json")
 
-getContentFromURL :: CF.Credentials -> String -> IO (Maybe Content)
+getContentFromURL :: CF.Credentials -> String -> IO (Maybe ZH.Content)
 getContentFromURL creds url = do
   let urlHash = sha256 url
   let urlPath = "/content/content-by-url/" ++ urlHash ++ ".txt"
@@ -49,7 +50,7 @@ getContentFromURL creds url = do
     sha256 :: String -> String
     sha256 x = show (Crypto.hash $ C.pack x :: Crypto.Digest Crypto.SHA256)
 
-contentById :: String -> Handler Content
+contentById :: String -> Handler ZH.Content
 contentById id = do
   maybeContent <- IO.liftIO $ getContentFromFile id
   case maybeContent of
@@ -57,7 +58,7 @@ contentById id = do
     Just content -> return content
 
 -- TODO: Use redirect to `contentById` instead:
-contentByURL :: CF.Credentials -> Maybe String -> Handler Content
+contentByURL :: CF.Credentials -> Maybe String -> Handler ZH.Content
 contentByURL creds url = case url of
   Nothing  -> Either.left S.err400{
     errBody="Please provide an ID or `url` query parameter."
