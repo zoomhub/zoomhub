c8a422c2e8189567eff50320113258cc9ac3a850
Daniel Gasienica <daniel@gasienica.ch>
2015-12-10 00:46:45 -0800

    Use `UTCTime` for `activeAt` and `finishedAt`

--- ZoomHub/Types/Content.hs
+++ ZoomHub/Types/Content.hs
@@ -7,6 +7,7 @@ import Data.Aeson as Aeson
 import Data.Aeson.Casing
 import ZoomHub.Types.DeepZoomImage
 
+import qualified Data.Time.Clock as DTC
 import qualified GHC.Generics as GHC
 
 
@@ -21,8 +22,8 @@ data Content = Content
   , contentMime :: String -- Use proper MIME type
   , contentSize :: Integer
   , contentActive :: Bool
-  , contentActiveAt :: String -- Use proper date type
-  , contentFinishedAt :: String -- Use proper date type
+  , contentActiveAt :: DTC.UTCTime
+  , contentFinishedAt :: DTC.UTCTime
   , contentDzi :: Maybe DeepZoomImage
   } deriving (Eq, Show, GHC.Generic)
 
