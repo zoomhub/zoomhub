363458468f15eca911af2462fe43aac206a2163d
Daniel Gasienica <daniel@gasienica.ch>
2015-12-19 23:40:27 -0800

    Separate internal serialization from public
    Internally, we store more information than we expose externally, e.g.
    `Content.activeAt`, etc.
    
    Convert from internal to public using `fromInternal`.

--- ZoomHub/Types/DeepZoomImage.hs
+++ ZoomHub/Types/DeepZoomImage.hs
@@ -7,10 +7,13 @@ import Data.Aeson as Aeson
 import Data.Aeson.Casing
 
 import qualified GHC.Generics as GHC
+import qualified ZoomHub.Types.Internal.Content as IC
+import qualified ZoomHub.Types.Internal.DeepZoomImage as ID
 
 
 data DeepZoomImage = DeepZoomImage
-  { dziWidth :: Integer
+  { dziUrl :: String
+  , dziWidth :: Integer
   , dziHeight :: Integer
   , dziTileSize :: Integer
   , dziTileOverlap :: Integer
@@ -21,3 +24,13 @@ instance Aeson.ToJSON DeepZoomImage where
    toJSON = genericToJSON $ aesonPrefix camelCase
 instance Aeson.FromJSON DeepZoomImage where
    parseJSON = genericParseJSON $ aesonPrefix camelCase
+
+fromInternal :: IC.ContentId -> ID.DeepZoomImage -> DeepZoomImage
+fromInternal cid dzi = DeepZoomImage
+  { dziUrl = "http://content.zoomhub.net/dzis/" ++ (show cid) ++ ".dzi"
+  , dziWidth = ID.dziWidth dzi
+  , dziHeight = ID.dziHeight dzi
+  , dziTileSize = ID.dziTileSize dzi
+  , dziTileOverlap = ID.dziTileOverlap dzi
+  , dziTileFormat = ID.dziTileFormat dzi
+  }
