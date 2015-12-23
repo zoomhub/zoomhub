9b91fc5d17bcb5f35cc54b63dc0859d948f1c993
Daniel Gasienica <daniel@gasienica.ch>
2015-12-23 01:06:52 -0800

    Extract `Config`

--- Main.hs
+++ Main.hs
@@ -1,46 +1,24 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
 
+import qualified Control.Concurrent.STM as STM
+import qualified Control.Monad.IO.Class as IO
 import qualified Data.Either as E
-import qualified Data.Maybe as M
-import qualified GHC.Generics as GHC
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified System.Environment as System
 import qualified System.Envy as Env
-import qualified ZoomHub.API as ZoomHub
-import qualified ZoomHub.Rackspace.CloudFiles as CF
-
-
--- Config: Rackspace
-data RackspaceConfig = RackspaceConfig
-  { raxUsername :: String -- RACKSPACE_USERNAME
-  , raxApiKey :: String   -- RACKSPACE_API_KEY
-  } deriving (GHC.Generic, Show)
-
--- Default configuration will be used for fields that could not be
--- retrieved from the environment:
-instance Env.DefConfig RackspaceConfig where
-  defConfig = RackspaceConfig{raxUsername="zoomingservice", raxApiKey=""}
-
-instance Env.FromEnv RackspaceConfig where
-  fromEnv = Env.gFromEnvCustom Env.Option
-    { Env.dropPrefixCount=3
-    , Env.customPrefix="RACKSPACE"
-    }
+import qualified ZoomHub.API as ZH
+import qualified ZoomHub.Config as C
 
 -- Main
-defaultPort :: Int
-defaultPort = 8000
-
 main :: IO ()
 main = do
   maybePort <- System.lookupEnv "PORT"
   raxConfig <- Env.decodeEnv
   case raxConfig of
-    E.Right config ->
-      let port = read $ M.fromMaybe (show defaultPort) maybePort in
-      Warp.run port (ZoomHub.app credentials)
-      where
-        username = raxUsername config
-        apiKey = raxApiKey config
-        credentials = CF.Credentials username apiKey
+    E.Right rackspace -> do
+      lastId <- IO.liftIO $ STM.atomically (STM.newTVar 0)
+      let port = maybe C.defaultPort read maybePort
+      let config = C.Config{..}
+      Warp.run (fromIntegral port) (ZH.app config)
     E.Left message -> error $ "Failed to read environment: " ++ message
