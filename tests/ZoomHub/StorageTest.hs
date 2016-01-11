41b3321c8df7aaa7a25af0baccd8acb122e63836
Daniel Gasienica <daniel@gasienica.ch>
2016-01-10 22:16:22 -0800

    Add test for `ContentId`

--- tests/ZoomHub/StorageTest.hs
+++ tests/ZoomHub/StorageTest.hs
@@ -4,5 +4,5 @@ import Data.List (isInfixOf)
 import Test.QuickCheck ((==>), Property)
 import ZoomHub.Storage
 
-prop_inversion :: String -> Property
-prop_inversion x = not ("_" `isInfixOf` x) ==> (toId . toFilename) x == x
+propInversion :: String -> Property
+propInversion x = not ("_" `isInfixOf` x) ==> (toId . toFilename) x == x
