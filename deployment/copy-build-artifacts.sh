0eb703b31e089578ac345229ffccfa79bdc791bd
Daniel Gasienica <daniel@gasienica.ch>
2016-02-14 16:26:12 -0800

    Ignore directories in `find` result

--- deployment/copy-build-artifacts.sh
+++ deployment/copy-build-artifacts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-zoomhub=$(find .stack-work/dist -name zoomhub | tr -d '\n')
+zoomhub=$(find .stack-work/dist -type f -name zoomhub | tr -d '\n')
 cp -R public $CIRCLE_ARTIFACTS
 cp "$zoomhub" $CIRCLE_ARTIFACTS
