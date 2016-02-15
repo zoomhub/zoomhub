9c8438cbe30fb3e5f8d78b7e33b096a5f82712c5
Daniel Gasienica <daniel@gasienica.ch>
2016-02-14 17:23:11 -0800

    Resume index creation after `j`

--- scripts/create-url-index.sh
+++ scripts/create-url-index.sh
@@ -6,7 +6,7 @@
 
 CONTENT_BY_ID='/mnt/datavolume/zoomhub/data/content-by-id'
 CONTENT_BY_URL='/mnt/datavolume/zoomhub/data/content-by-url'
-for target in $CONTENT_BY_ID/{{6..9},{a..z},{A..Z}}*.json ;
+for target in $CONTENT_BY_ID/{{k..z},{A..Z}}*.json ;
 do
   content=$(cat "$target")
   contentID=$(printf '%s' "$content" | json 'id')
