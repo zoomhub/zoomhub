9f27861e1497649992ab4e1fbd98e9f07aab3c26
Daniel Gasienica <daniel@gasienica.ch>
2016-02-14 16:31:12 -0800

    Copy `public` recursively using `scp`

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 scp $CIRCLE_ARTIFACTS/zoomhub root@104.130.230.137:/var/apps/zoomhub
-scp $CIRCLE_ARTIFACTS/public root@104.130.230.137:/var/apps/zoomhub
+scp -r $CIRCLE_ARTIFACTS/public root@104.130.230.137:/var/apps/zoomhub
