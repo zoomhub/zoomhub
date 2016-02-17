4e21497b7ba9387c1372ef46bf2409e0bd2c8b86
Daniel Gasienica <daniel@gasienica.ch>
2016-02-16 21:54:58 -0800

    Document `keter-bundle.sh`

--- deployment/keter-bundle.sh
+++ deployment/keter-bundle.sh
@@ -3,15 +3,28 @@
 # Creates Keter bundle of app.
 # See: https://github.com/snoyberg/keter#bundles
 
+
+# Find app binary:
 zoomhub=$(find .stack-work/dist -type f -name zoomhub | tr -d '\n')
 
+# Remove existing bundle:
 rm -rf .keter-bundle
+
+# Create container:
 mkdir -p .keter-bundle
+
+# Copy contents:
 cp "$zoomhub" .keter-bundle
 cp -R public .keter-bundle
 cp -R config .keter-bundle
+
+# Create bundle:
 cd .keter-bundle
 tar czfv zoomhub.keter zoomhub public config
 cd -
+
+# Move bundle to root:
 mv .keter-bundle/zoomhub.keter .
+
+# Remove bundle
 rm -rf .keter-bundle
