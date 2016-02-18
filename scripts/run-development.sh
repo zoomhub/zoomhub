568a87cbaf1cb9c72f2513eb4feafbf0e926c747
Daniel Gasienica <daniel@gasienica.ch>
2016-02-17 22:06:19 -0800

    Fix PID file path

--- scripts/run-development.sh
+++ scripts/run-development.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-[ -f /tmp/zoomhub.pid ] && kill $(cat zoomhub.pid)
+[ -f ./zoomhub.pid ] && kill $(cat zoomhub.pid)
 HASHIDS_SALT='DEVELOPMENT-ONLY' stack exec zoomhub &
 echo $! > zoomhub.pid
