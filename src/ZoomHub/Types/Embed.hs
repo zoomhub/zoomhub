4981bb913e1c4cc9ed2722b9c576f38a8f7886bc
Daniel Gasienica <daniel@gasienica.ch>
2016-02-24 21:32:36 -0800

    Split `Embed` into `EmbedParam` and `Embed`

--- src/ZoomHub/Types/Embed.hs
+++ src/ZoomHub/Types/Embed.hs
@@ -1,58 +1,50 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module ZoomHub.Types.Embed
   ( Embed
-  , fromString
+  , EmbedDimension(..)
+  , mkEmbed
+  , embedContentId
+  , embedWidth
+  , embedHeight
+  , embedBody
+  , embedDZI
   ) where
 
-import           Prelude                          hiding (fromInteger)
+import           Prelude                              hiding (fromInteger)
 
-import qualified Data.ByteString.Char8            as BC
-import qualified Data.Text                        as T
-import           GHC.Generics                     (Generic)
-import           Lucid                            (ToHtml, toHtml, toHtmlRaw)
-import           Network.HTTP.Types               (decodePath)
-import           Servant                          (FromText, fromText)
-import           System.FilePath.Posix            (splitExtension)
+-- import qualified Data.ByteString.Char8            as BC
+-- import qualified Data.Text                        as T
+import           GHC.Generics                         (Generic)
+import           Lucid                                (ToHtml, toHtml,
+                                                       toHtmlRaw)
 
-import           ZoomHub.Types.Internal.ContentId (ContentId)
-import qualified ZoomHub.Types.Internal.ContentId as ContentId
+import           ZoomHub.Types.Internal.ContentId     (ContentId)
+import           ZoomHub.Types.Internal.DeepZoomImage
 
 data EmbedDimension = Auto | Pixels Integer
   deriving (Eq, Generic, Show)
 
 data Embed = Embed
   { embedContentId :: ContentId
+  , embedBody      :: String
+  , embedDZI       :: DeepZoomImage
   , embedWidth     :: Maybe EmbedDimension
   , embedHeight    :: Maybe EmbedDimension
   } deriving (Eq, Generic, Show)
 
-fromString :: String -> Maybe Embed
-fromString s = case maybeContentId of
-    Nothing -> Nothing
-    Just embedContentId ->
-      Just Embed { embedContentId, embedWidth, embedHeight }
-  where
-    embedWidth = Nothing  -- TODO
-    embedHeight = Nothing -- TODO
-    pathAndQuery = decodePath (BC.pack s)
-    pathSegments = fst pathAndQuery
-    maybeContentId = case pathSegments of
-      (p:[]) ->
-        let idParts = splitExtension . T.unpack $ p in
-        case idParts of
-          (cId, ".js") -> Just $ ContentId.fromString cId
-          _ -> Nothing
-      _ -> Nothing
-
--- Text
-instance FromText Embed where
-  fromText t = fromString s
-    where s = T.unpack t
+mkEmbed :: ContentId ->
+           String ->
+           DeepZoomImage ->
+           Maybe EmbedDimension ->
+           Maybe EmbedDimension ->
+           Embed
+mkEmbed embedContentId embedBody embedDZI embedWidth embedHeight = Embed{..}
 
 -- HTML
 instance ToHtml Embed where
-  toHtml embed = toHtml $ show embed
+  toHtml embed = toHtml . embedBody $ embed
   toHtmlRaw = toHtml
---
+++ src/ZoomHub/Types/EmbedParam.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module ZoomHub.Types.EmbedParam
+  ( EmbedParam
+  , fromString
+  , embedParamContentId
+  , embedParamWidth
+  , embedParamHeight
+  ) where
+
+import           Prelude                          hiding (fromInteger)
+
+import qualified Data.ByteString.Char8            as BC
+import qualified Data.Text                        as T
+import           GHC.Generics                     (Generic)
+import           Network.HTTP.Types               (decodePath)
+import           Servant                          (FromText, fromText)
+import           System.FilePath.Posix            (splitExtension)
+
+import           ZoomHub.Types.Embed              (EmbedDimension (..))
+import           ZoomHub.Types.Internal.ContentId (ContentId)
+import qualified ZoomHub.Types.Internal.ContentId as ContentId
+
+data EmbedParam = EmbedParam
+  { embedParamContentId :: ContentId
+  , embedParamWidth     :: Maybe EmbedDimension
+  , embedParamHeight    :: Maybe EmbedDimension
+  } deriving (Eq, Generic, Show)
+
+fromString :: String -> Maybe EmbedParam
+fromString s = case maybeContentId of
+    Nothing -> Nothing
+    Just embedParamContentId ->
+      Just EmbedParam { embedParamContentId, embedParamWidth, embedParamHeight }
+  where
+    embedParamWidth = Nothing  -- TODO
+    embedParamHeight = Nothing -- TODO
+    pathAndQuery = decodePath (BC.pack s)
+    pathSegments = fst pathAndQuery
+    maybeContentId = case pathSegments of
+      (p:[]) ->
+        let idParts = splitExtension . T.unpack $ p in
+        case idParts of
+          (cId, ".js") -> Just $ ContentId.fromString cId
+          _ -> Nothing
+      _ -> Nothing
+
+-- Text
+instance FromText EmbedParam where
+  fromText t = fromString s
+    where s = T.unpack t
