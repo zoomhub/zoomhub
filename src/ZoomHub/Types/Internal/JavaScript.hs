d55f2ca9f1468cf2358ab2502bdc7cddcbd805f7
Daniel Gasienica <daniel@gasienica.ch>
2016-02-26 00:53:04 -0800

    Add custom `JavaScript` MIME type for embeds

---
+++ src/ZoomHub/Types/Internal/JavaScript.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+
+module ZoomHub.Types.Internal.JavaScript
+  ( ToJS
+  , JavaScript
+  , toJS
+  ) where
+
+import qualified Data.ByteString.Lazy.Char8 as BC
+import           Data.Typeable              (Typeable)
+import           Network.HTTP.Media         ((//), (/:))
+import           Servant.API.ContentTypes   (Accept (..), MimeRender (..))
+
+data JavaScript deriving Typeable
+
+-- | @application/javascript;charset=utf-8@
+instance Accept JavaScript where
+    contentType _ = "application" // "javascript" /: ("charset", "utf-8")
+
+instance ToJS a => MimeRender JavaScript a where
+    mimeRender _ = BC.pack . toJS
+
+-- | A type that can be converted to @application/javascript@
+class ToJS a where
+  toJS :: a -> String
+
+instance ToJS String where
+  toJS = id
