62e15a98791e29694e9c770443f62b3cb2bd0e6d
Daniel Gasienica <daniel@gasienica.ch>
2016-02-26 00:54:50 -0800

    Add basic support for rendering JavaScript embed
    TODO: Wire up DZI data and OpenSeadragon viewer metadata.

--- src/ZoomHub/Types/Embed.hs
+++ src/ZoomHub/Types/Embed.hs
@@ -12,39 +12,91 @@ module ZoomHub.Types.Embed
   , embedHeight
   , embedBody
   , embedDZI
+  , tag
   ) where
 
-import           Prelude                              hiding (fromInteger)
-
--- import qualified Data.ByteString.Char8            as BC
--- import qualified Data.Text                        as T
+import           Data.List                            (intersperse)
+import           Data.Maybe                           (fromMaybe)
 import           GHC.Generics                         (Generic)
-import           Lucid                                (ToHtml, toHtml,
-                                                       toHtmlRaw)
 
-import           ZoomHub.Types.Internal.ContentId     (ContentId)
+import           ZoomHub.Types.Internal.ContentId     (ContentId, unId)
+import           ZoomHub.Types.Internal.JavaScript     (ToJS, toJS, JavaScript)
 import           ZoomHub.Types.Internal.DeepZoomImage
 
+
+-- Constants
+cssClassName :: String
+cssClassName = "__seadragon"
+
+style :: Maybe EmbedDimension -> Maybe EmbedDimension -> String
+style maybeWidth maybeHeight = concat . intersperse " " $
+  [ "background: black;"
+  , "border: 1px solid black; "
+  , "color: white;"
+  , "height: " ++ toCSS height ++ ";"
+  , "margin: 0;"
+  , "padding: 0;"
+  , "width: " ++ toCSS width ++ ";"
+  ]
+  where
+    height = fromMaybe (Pixels 200) maybeHeight
+    width = fromMaybe (Auto) maybeWidth
+
+-- Types
 data EmbedDimension = Auto | Pixels Integer
   deriving (Eq, Generic, Show)
 
+toCSS :: EmbedDimension -> String
+toCSS Auto = "auto"
+toCSS (Pixels n) = show n ++ "px"
+
 data Embed = Embed
-  { embedContentId :: ContentId
+  { embedId        :: Integer
+  , embedContentId :: ContentId
   , embedBody      :: String
   , embedDZI       :: DeepZoomImage
   , embedWidth     :: Maybe EmbedDimension
   , embedHeight    :: Maybe EmbedDimension
   } deriving (Eq, Generic, Show)
 
-mkEmbed :: ContentId ->
+mkEmbed :: Integer ->
+           ContentId ->
            String ->
            DeepZoomImage ->
            Maybe EmbedDimension ->
            Maybe EmbedDimension ->
            Embed
-mkEmbed embedContentId embedBody embedDZI embedWidth embedHeight = Embed{..}
+mkEmbed embedId embedContentId embedBody embedDZI embedWidth embedHeight =
+  Embed{..}
 
 -- HTML
-instance ToHtml Embed where
-  toHtml embed = toHtml . embedBody $ embed
-  toHtmlRaw = toHtml
+concatPretty :: [String] -> String
+concatPretty = concat . intersperse "\n"
+
+concatScripts :: [String] -> String
+concatScripts = concat . intersperse ";\n"
+
+tag :: String -> [(String, String)] -> String
+tag name attrs = "<" ++ name ++ " " ++ concatMap attr attrs ++ ">" ++
+  "</" ++ name ++ ">"
+
+attr :: (String, String) -> String
+attr (name, value) = concat [name, "=\"", value, "\" "]
+
+instance ToJS Embed where
+  toJS embed = concatScripts $ [script, wrapper embed]
+    where
+      html = tag "div"
+        [ ("class", cssClassName)
+        , ("id", idAttr embed)
+        , ("style", style (embedWidth embed) (embedHeight embed))
+        ]
+      wrapper embed = concatPretty
+        [ "(function () {"
+        , "    document.write('" ++ html ++ "'});"
+        , "    OpenSeadragon({});"
+        , "}());"
+        ]
+      cId = unId . embedContentId $ embed
+      script = embedBody embed
+      idAttr embed = cssClassName ++ show (embedId embed)
