ee1afa2bc962b6deb84294a6321b1399dc966efb
Daniel Gasienica <daniel@gasienica.ch>
2016-02-28 15:41:24 -0800

    Create `ViewContent` type for view page

--- src/ZoomHub/Types/Content.hs
+++ src/ZoomHub/Types/Content.hs
@@ -18,13 +18,8 @@ import           Data.Aeson                       (ToJSON, Value (String),
                                                    genericToJSON, toJSON)
 import           Data.Aeson.Casing                (aesonPrefix, camelCase)
 import           Data.Maybe                       (fromJust)
-import           Data.Monoid                      ((<>))
 import qualified Data.Text                        as T
 import           GHC.Generics                     (Generic)
-import           Lucid                            (ToHtml, body_, content_,
-                                                   doctypehtml_, head_, meta_,
-                                                   name_, script_, src_, style_,
-                                                   title_, toHtml, toHtmlRaw)
 import           Network.URI                      (URI, parseRelativeReference,
                                                    relativeTo)
 
@@ -71,62 +66,6 @@ fromInternal baseURI contentBaseURI c = Content
 -- JSON
 instance ToJSON Content where
    toJSON = genericToJSON $ aesonPrefix camelCase
-
--- HTML
-concatPretty :: [T.Text] -> T.Text
-concatPretty = T.intercalate "\n"
-
--- TODO: Improve how we represent inline styles.
-styles :: T.Text
-styles = concatPretty
-  [ "html, body {"
-  , "  background-color: #000;"
-  , "  color: #fff;"
-  , "  font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;"
-  , "  height: 100%;"
-  , "  margin: 0;"
-  , "  padding: 0;"
-  , "}"
-  , ""
-  , ".__zoomhub {"
-  , "  height: 100% !important;"
-  , "}"
-  ]
-
--- TODO: Improve how we represent analytics code.
--- TODO: Pass through `UA-XXXXXXXX-X` Google Analytics ID.
-analyticsScript :: T.Text
-analyticsScript = concatPretty [
-    "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){",
-    "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),",
-    "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)",
-    "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');",
-    "",
-    "ga('create', 'UA-55808703-1', 'auto');",
-    "ga('_setDomainName', 'zoomhub.net');",
-    "ga('_setAllowLinker', true);",
-    "ga('send', 'pageview');"
-  ]
-
--- TODO: Pass through host:
-instance ToHtml Content where
-  toHtml content =
-      doctypehtml_ $
-        do head_ (do title_ (toHtml $ cId <> " — zoom.it")
-                     meta_ [name_ "viewport",
-                            content_ "width=device-width, initial-scale=1"]
-                     style_ styles
-                     script_ analyticsScript
-                     )
-           body_ $ script_ [src_ scriptURL] emptyBody
-    where
-      emptyBody :: T.Text
-      emptyBody = ""
-
-      scriptURL = "/" <> cId <> ".js?width=auto&height=100%"
-      cId = T.pack . unId $ contentId content
-
-  toHtmlRaw = toHtml
 
 -- Types
 newtype ContentShareURI = ContentShareURI { unContentShareURI :: URI }
---
+++ src/ZoomHub/Types/ViewContent.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+module ZoomHub.Types.ViewContent
+  ( ViewContent
+  , mkViewContent
+  )
+  where
+
+import           Data.Maybe                       (fromJust)
+import           Data.Monoid                      ((<>))
+import qualified Data.Text                        as T
+import           Lucid                            (ToHtml, body_, content_,
+                                                   doctypehtml_, head_, meta_,
+                                                   name_, script_, src_, style_,
+                                                   title_, toHtml, toHtmlRaw)
+import           Network.URI                      (URI, parseRelativeReference,
+                                                   relativeTo)
+
+import           ZoomHub.Types.BaseURI            (BaseURI, unBaseURI)
+import           ZoomHub.Types.Content            (Content, contentId)
+import           ZoomHub.Types.ContentBaseURI     (ContentBaseURI)
+import           ZoomHub.Types.DeepZoomImage      (DeepZoomImage)
+import qualified ZoomHub.Types.DeepZoomImage      as DZ
+import qualified ZoomHub.Types.Internal.Content   as Internal
+import           ZoomHub.Types.Internal.ContentId (ContentId, unId)
+
+data ViewContent = ViewContent
+  { vcContent :: Content
+  , vcBaseURI :: BaseURI
+  } deriving (Eq, Show)
+
+mkViewContent :: BaseURI -> Content -> ViewContent
+mkViewContent vcBaseURI vcContent = ViewContent{..}
+
+-- HTML
+concatPretty :: [T.Text] -> T.Text
+concatPretty = T.intercalate "\n"
+
+-- TODO: Improve how we represent inline styles.
+styles :: T.Text
+styles = concatPretty
+  [ "html, body {"
+  , "  background-color: black;"
+  , "  color: white;"
+  , "  font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;"
+  , "  height: 100%;"
+  , "  margin: 0;"
+  , "  padding: 0;"
+  , "}"
+  ]
+
+-- TODO: Improve how we represent analytics code.
+-- TODO: Pass through `UA-XXXXXXXX-X` Google Analytics ID.
+analyticsScript :: T.Text
+analyticsScript = concatPretty [
+    "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){",
+    "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),",
+    "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)",
+    "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');",
+    "",
+    "ga('create', 'UA-55808703-1', 'auto');",
+    "ga('_setDomainName', 'zoomhub.net');",
+    "ga('_setAllowLinker', true);",
+    "ga('send', 'pageview');"
+  ]
+
+instance ToHtml ViewContent where
+  toHtml vc =
+      doctypehtml_ $
+        do head_ (do title_ (toHtml $ "zoom.it — " <> cId)
+                     meta_ [name_ "viewport",
+                            content_ "width=device-width, initial-scale=1"]
+                     style_ styles
+                     script_ analyticsScript
+                     )
+           body_ $ script_ [src_ (T.pack $ show scriptURI)] emptyBody
+    where
+      content = vcContent vc
+      cId = unId $ contentId content
+      baseURI = vcBaseURI vc
+      scriptURI = scriptPath `relativeTo` unBaseURI baseURI
+      scriptPath = fromJust . parseRelativeReference $
+        "/" ++ cId ++ ".js?id=container&width=auto&height=100" ++ escapedPercent
+      escapedPercent = "%25"
+
+      emptyBody :: T.Text
+      emptyBody = ""
+
+  toHtmlRaw = toHtml
