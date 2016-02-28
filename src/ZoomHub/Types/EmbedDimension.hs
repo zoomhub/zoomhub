df2b1cf7d0e841d24a654fec9a435f9cdbe4f2c1
Daniel Gasienica <daniel@gasienica.ch>
2016-02-27 22:03:03 -0800

    Extract `EmbedDimension`

---
+++ src/ZoomHub/Types/EmbedDimension.hs
@@ -0,0 +1,15 @@
+module ZoomHub.Types.EmbedDimension
+  ( EmbedDimension(..)
+  , toCSSValue
+  ) where
+
+
+-- Type
+data EmbedDimension = Zero | Auto | Pixels Integer | Percentage Integer
+  deriving (Eq, Show)
+
+toCSSValue :: EmbedDimension -> String
+toCSSValue Auto = "auto"
+toCSSValue Zero = "0"
+toCSSValue (Pixels n) = show n ++ "px"
+toCSSValue (Percentage n) = show n ++ "%"
