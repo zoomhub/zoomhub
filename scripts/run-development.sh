58978a510545a89b0070fd55463ceb60c7788486
Daniel Gasienica <daniel@gasienica.ch>
2016-03-01 23:46:12 -0800

    Add attempt at shushing Mac OS X firewall

--- scripts/run-development.sh
+++ scripts/run-development.sh
@@ -4,6 +4,13 @@ if [[ -f ./zoomhub.pid ]] ; then
   kill $(cat zoomhub.pid)
 fi
 
+
+# # Find app binary:
+# zoomhub=$(find .stack-work/dist -type f -name zoomhub | tr -d '\n')
+
+# # Self-sign app to avoid constant Mac OS X firewall warnings:
+# codesign --force --sign zoomhub.net "$zoomhub"
+
 BASE_URI='http://localhost:8000' \
 HASHIDS_SALT='DEVELOPMENT-ONLY' \
   stack exec zoomhub &
