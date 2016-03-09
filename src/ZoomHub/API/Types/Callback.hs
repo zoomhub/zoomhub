0a83ac460c389c2212b3273abca06063510f1fe2
Daniel Gasienica <daniel@gasienica.ch>
2016-03-09 00:47:48 -0800

    Add infrastructure for JSONP support

---
+++ src/ZoomHub/API/Types/Callback.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module ZoomHub.API.Types.Callback
+  ( Callback
+  , unCallback
+  ) where
+
+import qualified Data.Text   as T
+import           Servant     (FromText, fromText)
+
+
+newtype Callback = Callback { unCallback :: String } deriving (Eq, Show)
+
+-- Text
+-- TODO: Disallow invalid JavaScript identifiers:
+instance FromText Callback where
+  fromText = Just . Callback . T.unpack
