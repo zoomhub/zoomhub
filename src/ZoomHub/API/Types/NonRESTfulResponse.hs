ded5a26258f3004d2ea2ab3f81af24884be852a9
Daniel Gasienica <daniel@gasienica.ch>
2016-03-09 22:53:29 -0800

    Add `mkNonRESTful400` and `mkNonRESTful503`

--- src/ZoomHub/API/Types/NonRESTfulResponse.hs
+++ src/ZoomHub/API/Types/NonRESTfulResponse.hs
@@ -5,13 +5,17 @@ module ZoomHub.API.Types.NonRESTfulResponse
   ( NonRESTfulResponse
   , mkNonRESTful200
   , mkNonRESTful301
+  , mkNonRESTful400
+  , mkNonRESTful503
   ) where
 
 import           Data.Aeson                (ToJSON, object, toJSON, (.=))
 import qualified Data.Text                 as T
 import           Data.Text.Encoding        (decodeUtf8)
-import           Network.HTTP.Types.Status (Status, movedPermanently301, ok200,
-                                            statusCode, statusMessage)
+import           Network.HTTP.Types.Status (Status, badRequest400,
+                                            movedPermanently301, ok200,
+                                            serviceUnavailable503, statusCode,
+                                            statusMessage)
 import           Network.URI               (URI)
 
 data NonRESTfulResponse a = ToJSON a => NonRESTfulResponse
@@ -35,6 +39,22 @@ mkNonRESTful301 key body redirectLocation = NonRESTfulResponse
   , nrrBodyKey = key
   , nrrBody = body
   , nrrRedirectLocation = Just redirectLocation
+  }
+
+mkNonRESTful400 :: String -> NonRESTfulResponse String
+mkNonRESTful400 message = NonRESTfulResponse
+  { nrrStatus = badRequest400
+  , nrrBodyKey = "error"
+  , nrrBody = message
+  , nrrRedirectLocation = Nothing
+  }
+
+mkNonRESTful503 :: String -> NonRESTfulResponse String
+mkNonRESTful503 message = NonRESTfulResponse
+  { nrrStatus = serviceUnavailable503
+  , nrrBodyKey = "error"
+  , nrrBody = message
+  , nrrRedirectLocation = Nothing
   }
 
 -- JSON
