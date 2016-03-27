fecd3627ccd7ee082d8d2443980a968b3e42a28f
Daniel Gasienica <daniel@gasienica.ch>
2016-03-26 19:32:46 -0700

    Reuse connection across database queries

--- src/ZoomHub/Storage/SQLite.hs
+++ src/ZoomHub/Storage/SQLite.hs
@@ -5,36 +5,32 @@ module ZoomHub.Storage.SQLite
   , getByURL
   ) where
 
-import           Database.SQLite.Simple            (Only (Only), Query, close,
-                                                    open, query)
+import           Database.SQLite.Simple            (Connection, Only (Only),
+                                                    Query, query)
 
 import           ZoomHub.Storage.SQLite.Internal   (rowToContent)
-import           ZoomHub.Types.DatabasePath        (DatabasePath,
-                                                    unDatabasePath)
 import           ZoomHub.Types.Internal.Content    (Content)
 import           ZoomHub.Types.Internal.ContentId  (ContentId, unId)
 import           ZoomHub.Types.Internal.ContentURI (ContentURI)
 
 
 -- Public URL
-getById :: DatabasePath -> ContentId -> IO (Maybe Content)
-getById dbPath cId =
-  get dbPath "SELECT id, hashId, url, state, initializedAt,\
+getById :: Connection -> ContentId -> IO (Maybe Content)
+getById conn cId =
+  get conn "SELECT id, hashId, url, state, initializedAt,\
     \ activeAt, completedAt, mime, size,progress, dzi_width, dzi_height,\
     \ dzi_tileSize, dzi_tileOverlap, dzi_tileFormat FROM content\
     \ WHERE hashId = ?" (unId cId)
 
-getByURL :: DatabasePath -> ContentURI -> IO (Maybe Content)
-getByURL dbPath uri =
-    get dbPath "SELECT id, hashId, url, state, initializedAt, activeAt,\
+getByURL :: Connection -> ContentURI -> IO (Maybe Content)
+getByURL conn uri =
+    get conn "SELECT id, hashId, url, state, initializedAt, activeAt,\
       \ completedAt, mime, size,progress, dzi_width, dzi_height, dzi_tileSize,\
       \ dzi_tileOverlap, dzi_tileFormat FROM content WHERE url = ?" (show uri)
 
-get :: DatabasePath -> Query -> String -> IO (Maybe Content)
-get dbPath q param = do
-  conn    <- open (unDatabasePath dbPath)
+get :: Connection -> Query -> String -> IO (Maybe Content)
+get conn q param = do
   results <- query conn q (Only param)
-  close conn
   case results of
     (r:_) -> return . Just . rowToContent $ r
     _     -> return Nothing
