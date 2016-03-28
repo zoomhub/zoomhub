2823010b21f7a3ca55c778b85d9797d7829bf224
Daniel Gasienica <daniel@gasienica.ch>
2016-03-27 19:30:14 -0700

    Implement `mkContent` smart constructor

--- src/ZoomHub/Types/Content.hs
+++ src/ZoomHub/Types/Content.hs
@@ -1,11 +1,12 @@
 module ZoomHub.Types.Content
   ( Content(..)
+  , mkContent
   ) where
 
 import           Data.Time.Clock             (UTCTime)
 
 import           ZoomHub.Types.ContentId     (ContentId)
-import           ZoomHub.Types.ContentState  (ContentState)
+import           ZoomHub.Types.ContentState  (ContentState (Initialized))
 import           ZoomHub.Types.ContentURI    (ContentURI)
 import           ZoomHub.Types.DeepZoomImage (DeepZoomImage)
 
@@ -22,3 +23,17 @@ data Content = Content
   , contentProgress      :: Float
   , contentDzi           :: Maybe DeepZoomImage
   } deriving (Eq, Show)
+
+mkContent :: ContentId -> ContentURI -> Content
+mkContent cId uri = Content
+  { contentId = cId
+  , contentUrl = uri
+  , contentState = Initialized
+  , contentInitializedAt = Nothing
+  , contentActiveAt = Nothing
+  , contentCompletedAt = Nothing
+  , contentMime = Nothing
+  , contentSize = Nothing
+  , contentProgress = 0.0
+  , contentDzi = Nothing
+  }
