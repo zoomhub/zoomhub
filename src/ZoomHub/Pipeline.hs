67a450158783537eb3618fc8014ca8a755311a28
Daniel Gasienica <daniel@gasienica.ch>
2016-03-28 01:22:20 -0700

    Expand `process`

--- src/ZoomHub/Pipeline.hs
+++ src/ZoomHub/Pipeline.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ZoomHub.Pipeline
   ( process
   )
   where
 
 import           Control.Lens                             ((^.))
+import           Data.Aeson                               ((.=))
 import           Network.Wreq                             (get, responseBody)
 import           System.AtomicWrite.Writer.LazyByteString (atomicWriteFile)
 import           System.Directory                         (createDirectoryIfMissing)
@@ -13,11 +16,14 @@ import           System.Process                           (callProcess)
 
 import           ZoomHub.Config                           (Config)
 import qualified ZoomHub.Config                           as Config
+import           ZoomHub.Log.Logger                       (logInfo)
+import           ZoomHub.Storage.SQLite                   (markAsActive,
+                                                           markAsSuccess)
 import           ZoomHub.Types.Content                    (Content, contentId,
                                                            contentUrl)
 import           ZoomHub.Types.ContentId                  (unId)
+import           ZoomHub.Types.ContentURI                 (ContentURI)
 import           ZoomHub.Types.DeepZoomImage              (TileOverlap (TileOverlap1), TileSize (TileSize254))
-
 
 process :: Config -> Content -> IO Content
 process config content = do
@@ -25,19 +31,30 @@ process config content = do
     -- let rawPath = tmpDir </> rawContentId
     let rawPath = tempPath </> rawContentId
     createDirectoryIfMissing False tempPath
-    putStrLn $ "Downloading: " ++ show (contentUrl content)
-    downloadURL (show (contentUrl content)) rawPath
-    putStrLn $ "Creating DZI: " ++ rawContentId
+
+    logInfo "Mark content as active" ["id" .= contentId content]
+    activeContent <- markAsActive (Config.dbConnection config) content
+
+    logInfo "Download content"
+      [ "id" .= contentId content
+      , "url" .= contentUrl content
+      ]
+    downloadURL (contentUrl content) rawPath
+
+    logInfo "Create DZI" ["contentId" .= contentId content]
     createDZI rawPath (rawPath <.> ".dzi")
-    return content
+
+    logInfo "Set content state: 'completed:success'"
+      ["contentId" .= contentId content]
+    markAsSuccess (Config.dbConnection config) activeContent
   where
     tempPath = Config.dataPath config </> "content-raw"
     rawContentId = unId $ contentId content
     -- template = rawContentId ++ "."
 
-downloadURL :: String -> FilePath -> IO ()
+downloadURL :: ContentURI -> FilePath -> IO ()
 downloadURL url dest = do
-  resp <- get url
+  resp <- get (show url)
   let body = resp ^. responseBody
   atomicWriteFile dest body
 
