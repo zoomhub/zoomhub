b76183cc765bd211409488df42dee9197e62667f
Daniel Gasienica <daniel@gasienica.ch>
2016-04-02 14:14:55 -0700

    Import existing data into database

--- scripts/import-csv-sqlite3.sh
+++ scripts/import-csv-sqlite3.sh
@@ -2,22 +2,42 @@
 
 cd ~/zoom-it-data-git
 
-echo "===> Prepare fresh output directories"
+echo '===> Initialize output directories'
 rm -rf output
 mkdir -p output/{group1,group2}
 
-echo "===> Split files based on CSV (TSV) header line"
-csplit -n 4 -f './output/split-ContentInfo-' -k ~/zoom-it-data-git/ContentInfo.txt '/^#Attributes PartitionKey/' '{9999}' &> /dev/null
+echo '===> Split files based on CSV (TSV) header line'
+csplit -n 5 -f './output/split-ContentInfo-' -k ContentInfo.txt '/^#Attributes PartitionKey/' '{9999}' &> /dev/null
 
 # Print unique header lines:
-# cat  output/split-ContentInfo-* | grep '^#Attributes' | sort | uniq
+# cat output/split-ContentInfo-* | grep '^#Attributes' | sort | uniq
 # > #Attributes PartitionKey  RowKey  Timestamp AbuseLevel  AttributionLink AttributionText Blob  Error Id  Mime  NumAbuseReports Progress  Size  Stage Title Type  Url Version
 # > #Attributes PartitionKey  RowKey  Timestamp AbuseLevel  Blob  Error Id  Mime  NumAbuseReports Progress  Size  Stage Type  Url Version AttributionLink AttributionText Title
 
-echo "===> Group files by CSV (TSV) header line type"
+echo '===> Group files by CSV (TSV) header line type'
 group1=$(grep --files-with-matches -R '^#Attributes PartitionKey\tRowKey\tTimestamp\tAbuseLevel\tAttributionLink\tAttributionText\tBlob\tError\tId\tMime\tNumAbuseReports\tProgress\tSize\tStage\tTitle\tType\tUrl\tVersion\r$' output)
 group2=$(grep --files-with-matches -R '^#Attributes PartitionKey\tRowKey\tTimestamp\tAbuseLevel\tBlob\tError\tId\tMime\tNumAbuseReports\tProgress\tSize\tStage\tType\tUrl\tVersion\tAttributionLink\tAttributionText\tTitle\r$' output)
 mv $group1 output/group1
 mv $group2 output/group2
+
+echo '===> Import group 1 `ContentInfo` into SQLite'
+cat $(find output/group1 -type f | head -n 1) | head -n 1 > output/output-group1.csv
+cat output/group1/* | grep --invert '^#Attributes PartitionKey' >> output/output-group1.csv
+printf '.mode tab\n.import output/output-group1.csv ContentInfoGroup1' | sqlite3 output/zoomhub.sqlite3
+
+echo '===> Import group 2 `ContentInfo` into SQLite'
+cat $(find output/group2 -type f | head -n 1) | head -n 1 > output/output-group2.csv
+cat output/group2/* | grep --invert '^#Attributes PartitionKey' >> output/output-group2.csv
+printf '.mode tab\n.import output/output-group2.csv ContentInfoGroup2' | sqlite3 output/zoomhub.sqlite3
+
+echo '===> Import `ImageInfo` into SQLite'
+cat ImageInfo.txt | head -n 1 > output/ImageInfo.csv
+cat ImageInfo.txt | grep --invert '^#Attributes PartitionKey' >> output/ImageInfo.csv
+printf '.mode tab\n.import output/ImageInfo.csv ImageInfo' | sqlite3 output/zoomhub.sqlite3
+
+echo '===> Import `FlickrPhotoInfo` into SQLite'
+cat FlickrPhotoInfo.txt | head -n 1 > output/FlickrPhotoInfo.csv
+cat FlickrPhotoInfo.txt | grep --invert '^#Attributes PartitionKey' >> output/FlickrPhotoInfo.csv
+printf '.mode tab\n.import output/FlickrPhotoInfo.csv FlickrPhotoInfo' | sqlite3 output/zoomhub.sqlite3
 
 cd - > /dev/null
