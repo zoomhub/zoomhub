997aacc33e7010016b33be4b66021f3f78f88fda
Daniel Gasienica <daniel@gasienica.ch>
2016-04-01 23:20:57 -0700

    Do not print different headers

--- scripts/import-csv-sqlite3.sh
+++ scripts/import-csv-sqlite3.sh
@@ -7,7 +7,7 @@ mkdir -p output/{group1,group2}
 csplit -n 4 -f './output/split-ContentInfo-' -k ~/zoom-it-data-git/ContentInfo.txt '/^#Attributes PartitionKey/' '{9999}'
 
 # Print unique header lines:
-cat output/* | grep '^#Attributes' | sort | uniq
+# cat  output/split-ContentInfo-* | grep '^#Attributes' | sort | uniq
 # > #Attributes PartitionKey  RowKey  Timestamp AbuseLevel  AttributionLink AttributionText Blob  Error Id  Mime  NumAbuseReports Progress  Size  Stage Title Type  Url Version
 # > #Attributes PartitionKey  RowKey  Timestamp AbuseLevel  Blob  Error Id  Mime  NumAbuseReports Progress  Size  Stage Type  Url Version AttributionLink AttributionText Title
 
