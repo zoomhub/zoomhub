4ffb56c6fec6e36723d0719dc6bca54e84b15674
Daniel Gasienica <daniel@gasienica.ch>
2016-04-03 02:16:08 -0700

    Revert "Add basic database comparison script"
    This reverts commit 4edd3cc4adb76d47436aee415baaff6908c3d857.

--- scripts/compare-db.sql
+++
@@ -1,3 +0,0 @@
-ATTACH 'content-production.sqlite3' AS DB1;
-ATTACH 'zoomhub.sqlite3' AS DB2;
-SELECT * FROM DB1.content WHERE DB1.content.hashId NOT IN (SELECT DB2.content.hashId FROM DB2.content);
