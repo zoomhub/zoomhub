6b96bf7f8850a4652d301ff5d447d91ed5096f18
Daniel Gasienica <daniel@gasienica.ch>
2016-04-03 01:09:40 -0700

    Wrap MIME normalization in transaction

--- scripts/normalize-mime.hs
+++ scripts/normalize-mime.hs
@@ -3,16 +3,18 @@
 import           Codec.MIME.Parse       (parseMIMEType)
 import           Codec.MIME.Type        (showType)
 import           Data.Text              (Text)
-import           Database.SQLite.Simple (Connection, execute, open, query_)
+import           Database.SQLite.Simple (Connection, execute, open, query_,
+                                         withTransaction)
 
 main :: IO ()
 main = do
     conn <- open "./data/content-development.sqlite3"
-    results <-
-      query_ conn "SELECT id, mime FROM content" :: IO [(Integer, Maybe Text)]
-    putStrLn $ "Found records: " ++ show (length results)
-    mapM_ (normalizeMIMEType conn) results
-    putStrLn "Done"
+    withTransaction conn $ do
+      results <-
+        query_ conn "SELECT id, mime FROM content" :: IO [(Integer, Maybe Text)]
+      putStrLn $ "Found records: " ++ show (length results)
+      mapM_ (normalizeMIMEType conn) results
+      putStrLn "Done"
   where
     normalizeMIMEType :: Connection -> (Integer, Maybe Text) -> IO ()
     normalizeMIMEType conn (id_, maybeMIME) =
