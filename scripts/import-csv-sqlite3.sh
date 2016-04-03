93a9cee98623c6d0d6ee9a827410de1845315280
Daniel Gasienica <daniel@gasienica.ch>
2016-04-02 18:23:56 -0700

    Parametrize CSV (TSV) --> SQLite script

--- scripts/import-csv-sqlite3.sh
+++ scripts/import-csv-sqlite3.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-WORKING_DIRECTORY=$PWD
-cd ~/zoom-it-data-git
+ROOT="$1"
+
+if [[ ! -d "$ROOT" ]] ; then
+  echo 'Please provide the path to the data directory: `./scripts/import-csv-sqlite3.sh DATA_DIRECTORY'
+  exit 1
+fi
 
 echo '===> Initialize output directories'
-rm -rf output
-mkdir -p output/{group1,group2}
+rm -rf $ROOT/output
+mkdir -p $ROOT/output/{group1,group2}
 
 echo '===> Split files based on CSV (TSV) header line'
-csplit -n 5 -f './output/split-ContentInfo-' -k ContentInfo.txt '/^#Attributes PartitionKey/' '{9999}' &> /dev/null
+csplit -n 5 -f "$ROOT/output/split-ContentInfo-" -k $ROOT/ContentInfo.txt '/^#Attributes PartitionKey/' '{9999}' &> /dev/null
 
 # Print unique header lines:
 # cat output/split-ContentInfo-* | grep '^#Attributes' | sort | uniq
@@ -16,48 +20,49 @@ csplit -n 5 -f './output/split-ContentInfo-' -k ContentInfo.txt '/^#Attributes P
 # > #Attributes PartitionKey  RowKey  Timestamp AbuseLevel  Blob  Error Id  Mime  NumAbuseReports Progress  Size  Stage Type  Url Version AttributionLink AttributionText Title
 
 echo '===> Group files by CSV (TSV) header line type'
-group1=$(grep --files-with-matches -R '^#Attributes PartitionKey\tRowKey\tTimestamp\tAbuseLevel\tAttributionLink\tAttributionText\tBlob\tError\tId\tMime\tNumAbuseReports\tProgress\tSize\tStage\tTitle\tType\tUrl\tVersion\r$' output)
-group2=$(grep --files-with-matches -R '^#Attributes PartitionKey\tRowKey\tTimestamp\tAbuseLevel\tBlob\tError\tId\tMime\tNumAbuseReports\tProgress\tSize\tStage\tType\tUrl\tVersion\tAttributionLink\tAttributionText\tTitle\r$' output)
-mv $group1 output/group1
-mv $group2 output/group2
+group1=$(grep --files-with-matches -R '^#Attributes PartitionKey\tRowKey\tTimestamp\tAbuseLevel\tAttributionLink\tAttributionText\tBlob\tError\tId\tMime\tNumAbuseReports\tProgress\tSize\tStage\tTitle\tType\tUrl\tVersion\r$' $ROOT/output)
+group2=$(grep --files-with-matches -R '^#Attributes PartitionKey\tRowKey\tTimestamp\tAbuseLevel\tBlob\tError\tId\tMime\tNumAbuseReports\tProgress\tSize\tStage\tType\tUrl\tVersion\tAttributionLink\tAttributionText\tTitle\r$' $ROOT/output)
+mv $group1 $ROOT/output/group1
+mv $group2 $ROOT/output/group2
 
 echo '===> Initialize database'
-cat $WORKING_DIRECTORY/scripts/create-db.sql | sqlite3 output/zoomhub.sqlite3
+cat ./scripts/create-db.sql | sqlite3 $ROOT/output/zoomhub.sqlite3
 
 echo '===> Import `ContentInfo` (group 1) into SQLite'
-cat $(find output/group1 -type f | head -n 1) | head -n 1 > output/output-group1.csv
-cat output/group1/* | grep --invert '^#Attributes PartitionKey' >> output/output-group1.csv
-printf '.mode tab\n.import output/output-group1.csv ContentInfoGroup1' | sqlite3 output/zoomhub.sqlite3
+cat $(find $ROOT/output/group1 -type f | head -n 1) | head -n 1 > $ROOT/output/output-group1.csv
+cat $ROOT/output/group1/* | grep --invert '^#Attributes PartitionKey' >> $ROOT/output/output-group1.csv
+printf ".mode tab\n.import $ROOT/output/output-group1.csv ContentInfoGroup1" | sqlite3 $ROOT/output/zoomhub.sqlite3
 
 echo '===> Import `ContentInfo` (group 2) into SQLite'
-cat $(find output/group2 -type f | head -n 1) | head -n 1 > output/output-group2.csv
-cat output/group2/* | grep --invert '^#Attributes PartitionKey' >> output/output-group2.csv
-printf '.mode tab\n.import output/output-group2.csv ContentInfoGroup2' | sqlite3 output/zoomhub.sqlite3
+cat $(find $ROOT/output/group2 -type f | head -n 1) | head -n 1 > $ROOT/output/output-group2.csv
+cat $ROOT/output/group2/* | grep --invert '^#Attributes PartitionKey' >> $ROOT/output/output-group2.csv
+printf ".mode tab\n.import $ROOT/output/output-group2.csv ContentInfoGroup2" | sqlite3 $ROOT/output/zoomhub.sqlite3
 
 echo '===> Import `ImageInfo` into SQLite'
-cat ImageInfo.txt | head -n 1 > output/ImageInfo.csv
-cat ImageInfo.txt | grep --invert '^#Attributes PartitionKey' >> output/ImageInfo.csv
-printf '.mode tab\n.import output/ImageInfo.csv ImageInfo' | sqlite3 output/zoomhub.sqlite3
+cat $ROOT/ImageInfo.txt | head -n 1 > $ROOT/output/ImageInfo.csv
+cat $ROOT/ImageInfo.txt | grep --invert '^#Attributes PartitionKey' >> $ROOT/output/ImageInfo.csv
+printf ".mode tab\n.import $ROOT/output/ImageInfo.csv ImageInfo" | sqlite3 $ROOT/output/zoomhub.sqlite3
 
 echo '===> Import `FlickrPhotoInfo` into SQLite'
-cat FlickrPhotoInfo.txt | head -n 1 > output/FlickrPhotoInfo.csv
-cat FlickrPhotoInfo.txt | grep --invert '^#Attributes PartitionKey' >> output/FlickrPhotoInfo.csv
-printf '.mode tab\n.import output/FlickrPhotoInfo.csv FlickrPhotoInfo' | sqlite3 output/zoomhub.sqlite3
+cat $ROOT/FlickrPhotoInfo.txt | head -n 1 > $ROOT/output/FlickrPhotoInfo.csv
+cat $ROOT/FlickrPhotoInfo.txt | grep --invert '^#Attributes PartitionKey' >> $ROOT/output/FlickrPhotoInfo.csv
+printf ".mode tab\n.import $ROOT/output/FlickrPhotoInfo.csv FlickrPhotoInfo" | sqlite3 $ROOT/output/zoomhub.sqlite3
 
 echo '===> Check consistency of `ContentInfoGroup1` table'
-numMismatchingIdRowKeyGroup1=$(echo 'SELECT COUNT(*) FROM ContentInfoGroup1 WHERE RowKey <> Id;' | sqlite3 output/zoomhub.sqlite3)
+numMismatchingIdRowKeyGroup1=$(echo 'SELECT COUNT(*) FROM ContentInfoGroup1 WHERE RowKey <> Id;' | sqlite3 $ROOT/output/zoomhub.sqlite3)
 if [[ "$numMismatchingIdRowKeyGroup1" != '0' ]] ; then
   echo "Found $numMismatchingIdRowKeyGroup1 rows in \`ContentInfoGroup1\` that have mismatching \`Id\` and \`RowKey\` columns."
   exit 1
 fi
 
 echo '===> Check consistency of `ContentInfoGroup2` table'
-numMismatchingIdRowKeyGroup2=$(echo 'SELECT COUNT(*) FROM ContentInfoGroup2 WHERE RowKey <> Id;' | sqlite3 output/zoomhub.sqlite3)
+numMismatchingIdRowKeyGroup2=$(echo 'SELECT COUNT(*) FROM ContentInfoGroup2 WHERE RowKey <> Id;' | sqlite3 $ROOT/output/zoomhub.sqlite3)
 if [[ "$numMismatchingIdRowKeyGroup2" != '0' ]] ; then
   echo "Found $numMismatchingIdRowKeyGroup2 rows in \`ContentInfoGroup2\` that have mismatching \`Id\` and \`RowKey\` columns."
   exit 1
 fi
 
-open output/zoomhub.sqlite3
+echo '===> Import  `ContentInfoGroup1` and `ContentInfoGroup2` tables into `content`'
+cat ./scripts/import-legacy-data.sql | sqlite3 $ROOT/output/zoomhub.sqlite3
 
-cd - > /dev/null
+open $ROOT/output/zoomhub.sqlite3
