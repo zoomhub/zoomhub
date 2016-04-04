291ad12c5fd7f2cd6de0fc00d28089ee6c8ddd1f
Daniel Gasienica <daniel@gasienica.ch>
2016-04-03 17:54:37 -0700

    Update set queries and introduce `error` and `type`

--- src/ZoomHub/Storage/SQLite.hs
+++ src/ZoomHub/Storage/SQLite.hs
@@ -21,6 +21,7 @@ import           Data.Monoid                    ((<>))
 import           Data.Set                       (Set)
 import qualified Data.Set                       as Set
 import           Data.String                    (IsString (fromString))
+import           Data.Text                      (Text)
 import           Data.Time.Clock                (UTCTime, getCurrentTime)
 import           Database.SQLite.Simple         (Connection, NamedParam ((:=)),
                                                  Only (Only), Query, execute,
@@ -36,15 +37,17 @@ import           ZoomHub.Log.Logger             (logWarning)
 import           ZoomHub.Types.Content          (Content (Content),
                                                  contentActiveAt,
                                                  contentCompletedAt, contentDZI,
-                                                 contentId,
+                                                 contentError, contentId,
                                                  contentInitializedAt,
                                                  contentMIME, contentProgress,
                                                  contentSize, contentState,
-                                                 contentURL, mkContent)
+                                                 contentType, contentURL,
+                                                 mkContent)
 import           ZoomHub.Types.ContentId        (ContentId, unId)
 import qualified ZoomHub.Types.ContentId        as ContentId
 import           ZoomHub.Types.ContentMIME      (ContentMIME)
 import           ZoomHub.Types.ContentState     (ContentState (Initialized, Active, CompletedSuccess, CompletedFailure))
+import           ZoomHub.Types.ContentType      (ContentType (Unknown, Image))
 import           ZoomHub.Types.ContentURI       (ContentURI)
 import           ZoomHub.Types.DeepZoomImage    (DeepZoomImage (DeepZoomImage),
                                                  TileFormat, TileOverlap,
@@ -93,39 +96,54 @@ getByURL conn uri =
 
 getNextUnprocessed :: Connection -> IO (Maybe Content)
 getNextUnprocessed conn =
-  get $ query conn "SELECT * FROM content WHERE state = ? AND\
-    \ initializedAt IS NULL ORDER BY id ASC LIMIT 1"
+  get $ query conn (selectContent <> "WHERE state = ? \
+    \ ORDER BY content.initializedAt ASC LIMIT 1")
     (Only Initialized)
 
 markAsActive :: Connection -> Content -> IO Content
 markAsActive conn content = do
-  now <- getCurrentTime
+  activeAt <- getCurrentTime
   let content' = content
         { contentState = Active
-        , contentActiveAt = Just now
+        , contentActiveAt = Just activeAt
         }
   withTransaction conn $
-    executeNamed conn "UPDATE content \
-      \ SET state = :state, activeAt = :activeAt WHERE hashId = :hashId"
+    executeNamed conn "\
+      \UPDATE content \
+      \ SET state = :state\
+      \   , activeAt = :activeAt\
+      \ WHERE hashId = :hashId"
       [ ":state" := contentState content'
       , ":activeAt" := contentActiveAt content'
       , ":hashId" := contentId content'
       ]
   return content'
 
-markAsFailure :: Connection -> Content -> IO Content
-markAsFailure conn content = do
-  now <- getCurrentTime
+markAsFailure :: Connection ->
+                 Content ->
+                 Maybe Text ->
+                 IO Content
+markAsFailure conn content maybeError = do
+  completedAt <- getCurrentTime
   let content' = content
         { contentState = CompletedFailure
-        , contentCompletedAt = Just now
+        , contentType = Unknown
+        , contentCompletedAt = Just completedAt
+        , contentError = maybeError
         }
   withTransaction conn $
-    executeNamed conn "UPDATE content \
-      \ SET state = :state, completedAt = :completedAt WHERE hashId = :hashId"
+    executeNamed conn "\
+      \UPDATE content\
+      \  SET state = :state\
+      \    , typeId = :typeId\
+      \    , completedAt = :completedAt\
+      \    , error = :error\
+      \  WHERE hashId = :hashId"
       [ ":state" := contentState content'
+      , ":typeId" := contentType content'
       , ":completedAt" := contentCompletedAt content'
       , ":hashId" := contentId content'
+      , ":error" := contentError content'
       ]
   return content'
 
@@ -136,39 +154,63 @@ markAsSuccess :: Connection ->
                  Integer ->
                  IO Content
 markAsSuccess conn content dzi maybeMIME size = do
-  now <- getCurrentTime
+  completedAt <- getCurrentTime
   let content' = content
         { contentState = CompletedSuccess
-        , contentCompletedAt = Just now
+        , contentType = Image -- TODO: Parametrize
+        , contentCompletedAt = Just completedAt
         , contentMIME = maybeMIME
         , contentSize = Just size
         , contentProgress = 1.0
+        , contentError = Nothing
         , contentDZI = Just dzi
         }
-  withTransaction conn $
-    executeNamed conn "UPDATE content \
-      \ SET state = :state\
-      \   , completedAt = :completedAt\
-      \   , mime = :mime\
-      \   , size = :size\
-      \   , progress = :progress\
-      \   , dzi_width = :dzi_width\
-      \   , dzi_height = :dzi_height\
-      \   , dzi_tileSize = :dzi_tileSize\
-      \   , dzi_tileOverlap = :dzi_tileOverlap\
-      \   , dzi_tileFormat = :dzi_tileFormat\
-      \ WHERE hashId = :hashId"
+  withTransaction conn $ do
+    executeNamed conn "\
+      \ UPDATE content \
+      \   SET state = :state\
+      \     , typeId = :typeId\
+      \     , completedAt = :completedAt\
+      \     , mime = :mime\
+      \     , size = :size\
+      \     , progress = :progress\
+      \     , error = :error\
+      \   WHERE hashId = :hashId"
       [ ":hashId" := contentId content'
+      , ":typeId" := contentType content'
       , ":state" := contentState content'
       , ":completedAt" := contentCompletedAt content'
       , ":mime" := contentMIME content'
       , ":size" := contentSize content'
       , ":progress" := contentProgress content'
-      , ":dzi_width" := Just (dziWidth dzi)
-      , ":dzi_height" := Just (dziHeight dzi)
-      , ":dzi_tileSize" := Just (dziTileSize dzi)
-      , ":dzi_tileOverlap" := Just (dziTileOverlap dzi)
-      , ":dzi_tileFormat" := Just (dziTileFormat dzi)
+      , ":error" := contentError content'
+      ]
+    executeNamed conn "\
+      \ INSERT OR REPLACE INTO image\
+      \    ( contentId\
+      \    , initializedAt\
+      \    , width\
+      \    , height\
+      \    , tileSize\
+      \    , tileOverlap\
+      \    , tileFormat\
+      \    )\
+      \ VALUES\
+      \   ( (SELECT id FROM content WHERE hashId = :hashId)\
+      \   , (SELECT initializedAt FROM image WHERE contentId =\
+      \       (SELECT id FROM content WHERE hashId = :hashId))\
+      \   , :image_width\
+      \   , :image_height\
+      \   , :image_tileSize\
+      \   , :image_tileOverlap\
+      \   , :image_tileFormat\
+      \   )"
+      [ ":hashId" := contentId content'
+      , ":image_width" := Just (dziWidth dzi)
+      , ":image_height" := Just (dziHeight dzi)
+      , ":image_tileSize" := Just (dziTileSize dzi)
+      , ":image_tileOverlap" := Just (dziTileOverlap dzi)
+      , ":image_tileFormat" := Just (dziTileFormat dzi)
       ]
   return content'
 
@@ -190,6 +232,7 @@ fieldNames :: [Query]
 fieldNames =
   [ "content.id"
   , "content.hashId"
+  , "content.typeId"
   , "content.url"
   , "content.state"
   , "content.initializedAt"
@@ -198,6 +241,7 @@ fieldNames =
   , "content.mime"
   , "content.size"
   , "content.progress"
+  , "content.error"
   , "image.width AS image_width"
   , "image.height AS image_height"
   , "image.tileSize AS image_tileSize"
@@ -215,9 +259,12 @@ insertFieldNames =
 
 queryFor :: String -> Query
 queryFor fieldName =
-  "SELECT " <> columns <> " FROM content\
-  \ JOIN image ON content.id = image.contentId\
-  \ WHERE " <> fromString fieldName <> " = ?"
+  selectContent <> "WHERE " <> fromString fieldName <> " = ?"
+
+selectContent :: Query
+selectContent =
+    "SELECT " <> columns <> " FROM content\
+    \ LEFT JOIN image ON content.id = image.contentId "
   where
     columns = intercalate ", " fieldNames
 
@@ -236,6 +283,7 @@ lastContentRowInsertIdQuery =
 data ContentRow = ContentRow
   { crId             :: Maybe Integer
   , crHashId         :: ContentId
+  , crType           :: ContentType
   , crURL            :: ContentURI
   , crState          :: ContentState
   , crInitializedAt  :: UTCTime
@@ -244,6 +292,7 @@ data ContentRow = ContentRow
   , crMIME           :: Maybe ContentMIME
   , crSize           :: Maybe Integer
   , crProgress       :: Float
+  , crError          :: Maybe Text
   , crDZIWidth       :: Maybe Integer
   , crDZIHeight      :: Maybe Integer
   , crDZITileSize    :: Maybe TileSize
@@ -255,6 +304,7 @@ instance FromRow ContentRow where
   fromRow = ContentRow <$>
     field <*> -- id
     field <*> -- hashId
+    field <*> -- type
     field <*> -- url
     field <*> -- state
     field <*> -- initializedAt
@@ -263,6 +313,7 @@ instance FromRow ContentRow where
     field <*> -- mime
     field <*> -- size
     field <*> -- progress
+    field <*> -- error
     field <*> -- dziWidth
     field <*> -- dziHeight
     field <*> -- dziTileSize
@@ -273,6 +324,7 @@ instance ToRow ContentRow where
   toRow (ContentRow{..}) =
     [ toField crId
     , toField crHashId
+    , toField crType
     , toField crURL
     , toField crState
     -- , toField crInitializedAt -- Omitted due to SQLite default value
@@ -281,6 +333,7 @@ instance ToRow ContentRow where
     , toField crMIME
     , toField crSize
     , toField crProgress
+    , toField crError
     , toField crDZIWidth
     , toField crDZIHeight
     , toField crDZITileSize
@@ -291,6 +344,7 @@ instance ToRow ContentRow where
 rowToContent :: ContentRow -> Content
 rowToContent cr = Content
     { contentId = crHashId cr
+    , contentType = crType cr
     , contentURL = crURL cr
     , contentState = crState cr
     , contentInitializedAt = crInitializedAt cr
@@ -299,6 +353,7 @@ rowToContent cr = Content
     , contentMIME = crMIME cr
     , contentSize = crSize cr
     , contentProgress = crProgress cr
+    , contentError = crError cr
     , contentDZI = maybeDZI
     }
   where
@@ -319,6 +374,7 @@ contentToRow :: Integer -> Content -> ContentRow
 contentToRow id_ c = ContentRow
     { crId = Just id_
     , crHashId = contentId c
+    , crType = contentType c
     , crURL = contentURL c
     , crState = contentState c
     , crInitializedAt = contentInitializedAt c
@@ -327,6 +383,7 @@ contentToRow id_ c = ContentRow
     , crMIME = contentMIME c
     , crSize = contentSize c
     , crProgress = contentProgress c
+    , crError = contentError c
     , crDZIWidth = dziWidth <$> dzi
     , crDZIHeight = dziHeight <$> dzi
     , crDZITileSize = dziTileSize <$> dzi
