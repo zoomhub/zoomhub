5d031daceaf5ba640910ef1c35cc70c5b6dc510a
Daniel Gasienica <daniel@gasienica.ch>
2016-04-06 00:41:12 -0700

    Implement content creation pipeline

--- src/ZoomHub/Pipeline.hs
+++ src/ZoomHub/Pipeline.hs
@@ -7,25 +7,39 @@ module ZoomHub.Pipeline
 
 import           Codec.MIME.Parse                         (parseMIMEType)
 import qualified Codec.MIME.Type                          as MIME
+import           Control.Exception                        (SomeException, catch)
 import           Control.Lens                             ((^.))
+import           Control.Monad                            (forM_)
 import           Data.Aeson                               ((.=))
+import           Data.List                                (stripPrefix)
 import           Data.Monoid                              ((<>))
+import qualified Data.Text                                as T
 import           Data.Text.Encoding                       (decodeUtf8)
 import           Network.Wreq                             (get, responseBody,
                                                            responseHeader)
 import           System.AtomicWrite.Writer.LazyByteString (atomicWriteFile)
 import           System.Directory                         (createDirectoryIfMissing,
                                                            listDirectory)
-import           System.FilePath                          (dropExtension, (<.>),
+import           System.FilePath                          (addTrailingPathSeparator,
+                                                           dropExtension, (<.>),
                                                            (</>))
--- import           System.IO.Temp                           (withTempDirectory)
+import           System.IO.Temp                           (withTempDirectory)
 import           System.Posix                             (fileSize,
                                                            getFileStatus)
 import           System.Process                           (callProcess)
 
-import           ZoomHub.Config                           (Config)
+import           ZoomHub.Config                           (Config,
+                                                           RackspaceConfig,
+                                                           raxApiKey,
+                                                           raxContainer,
+                                                           raxUsername)
 import qualified ZoomHub.Config                           as Config
-import           ZoomHub.Log.Logger                       (logInfo)
+import           ZoomHub.Log.Logger                       (logError, logInfo)
+import           ZoomHub.Rackspace.CloudFiles             (ObjectName,
+                                                           getMetadata,
+                                                           mkCredentials,
+                                                           parseObjectName,
+                                                           putContent)
 import           ZoomHub.Storage.SQLite                   (markAsActive,
                                                            markAsFailure,
                                                            markAsSuccess)
@@ -35,22 +49,37 @@ import           ZoomHub.Types.ContentId                  (unId)
 import           ZoomHub.Types.ContentMIME                (ContentMIME (ContentMIME))
 import           ZoomHub.Types.ContentURI                 (ContentURI)
 import           ZoomHub.Types.DeepZoomImage              (DeepZoomImage, TileFormat (JPEG, PNG), TileOverlap (TileOverlap1), TileSize (TileSize254),
+                                                           dziTileFormat,
                                                            fromXML)
 
 process :: Config -> Content -> IO Content
-process config content = do
-    createDirectoryIfMissing True tempPath
+process config content =
+    catch (unsafeProcess config content) $ \e -> do
+      let errorMessage = Just . T.pack . show $ (e :: SomeException)
+      logInfo "Process content: failure"
+        [ "id" .= contentId content
+        , "error" .= errorMessage
+        ]
+      markAsFailure conn content errorMessage
+  where
+    conn = Config.dbConnection config
 
-  -- withTempDirectory tempPath template $ \tmpDir -> do
-    let rawPath = tempPath </> rawContentId
-    -- let rawPath = tmpDir </> rawContentId
+unsafeProcess :: Config -> Content -> IO Content
+unsafeProcess config content = do
+  let tempPath = Config.dataPath config </> "content-raw"
+  createDirectoryIfMissing True tempPath
+
+  withTempDirectory tempPath template $ \tmpDir -> do
+    let rawPath = tmpDir </> rawContentId
         dziPath = rawPath <.> ".dzi"
 
     logInfo "Create temporary working directory"
-      ["id" .= contentId content, "path" .= tempPath]
-    createDirectoryIfMissing True tempPath
+      [ "id" .= contentId content
+      , "path" .= tempPath
+      ]
 
-    logInfo "Mark content as active" ["id" .= contentId content]
+    logInfo "Mark content as active"
+      [ "id" .= contentId content ]
     activeContent <- markAsActive conn content
 
     logInfo "Download content"
@@ -62,38 +91,30 @@ process config content = do
     rawSize <- getFileSize rawPath
 
     logInfo "Create DZI"
-      ["id" .= contentId content]
-    maybeDZI <- createDZI rawPath dziPath (toTileFormat maybeMIME)
+      [ "id" .= contentId content ]
+    dzi <- createDZI rawPath dziPath (toTileFormat maybeMIME)
 
     logInfo "Content metadata"
       [ "mime" .= maybeMIME
       , "size" .= rawSize
-      , "dzi" .= maybeDZI
+      , "dzi" .= dzi
       ]
 
-    case maybeDZI of
-      Just dzi -> do
-        logInfo "Upload DZI"
-          [ "id" .= contentId content
-          , "dzi" .= dzi
-          , "dziPath" .= dziPath
-          ]
-        uploadDZI dziPath
+    logInfo "Upload DZI"
+      [ "id" .= contentId content
+      , "dzi" .= dzi
+      , "dziPath" .= dziPath
+      ]
+    uploadDZI raxConfig tmpDir dziPath dzi
 
-        logInfo "Succeeded to process content"
-          ["id" .= contentId content]
-        markAsSuccess conn activeContent dzi maybeMIME rawSize
-
-      _ -> do
-        logInfo "Failed to process content"
-          ["id" .= contentId content]
-        let error_ = Nothing
-        markAsFailure conn activeContent error_
+    logInfo "Process content: success"
+      [ "id" .= contentId activeContent ]
+    markAsSuccess conn activeContent dzi maybeMIME rawSize
   where
+    raxConfig = Config.rackspace config
     conn = Config.dbConnection config
-    tempPath = Config.dataPath config </> "content-raw"
-    rawContentId = unId $ contentId content
-    -- template = rawContentId ++ "_"
+    rawContentId = unId (contentId content)
+    template = rawContentId ++ "-"
 
     getFileSize :: FilePath -> IO Integer
     getFileSize path = (toInteger . fileSize) <$> getFileStatus path
@@ -105,7 +126,7 @@ downloadURL url dest = do
   atomicWriteFile dest body
   return $ parseMIMEType . decodeUtf8 $ res ^. responseHeader "content-type"
 
-createDZI :: FilePath -> FilePath -> TileFormat -> IO (Maybe DeepZoomImage)
+createDZI :: FilePath -> FilePath -> TileFormat -> IO DeepZoomImage
 createDZI src dest tileFormat = do
   callProcess "vips"
     [ "dzsave"
@@ -116,13 +137,60 @@ createDZI src dest tileFormat = do
     , "--suffix=" <> toVIPSSuffix tileFormat
     , "--vips-progress"
     ]
-  dzi <- readFile dest
-  return $ fromXML dzi
+  xml <- readFile dest
+  case fromXML xml of
+    (Just dzi) -> return dzi
+    _ -> fail "Failed to create DZI"
 
-uploadDZI :: FilePath -> IO ()
-uploadDZI dziPath = do
-  tilePaths <- getDZITilePaths dziPath
-  print tilePaths
+uploadDZI :: RackspaceConfig ->
+             FilePath ->
+             FilePath ->
+             DeepZoomImage ->
+             IO ()
+uploadDZI raxConfig rootPath path dzi = do
+    meta <- getMetadata raxCreds
+    tilePaths <- getDZITilePaths path
+
+    -- Upload Tiles
+    forM_ tilePaths $ \tilePath ->
+      case toObjectName tilePath of
+        (Just tileObjectName) -> do
+          logInfo "Upload DZI tile"
+            [ "objectName" .= tileObjectName ]
+          _ <- putContent meta tilePath tileMIME container tileObjectName
+          return ()
+        _ -> logError "Invalid DZI tile object name"
+                [ "tilePath" .= tilePath
+                , "rootPath" .= rootPath
+                ]
+
+    -- Upload manifest
+    case toObjectName path of
+      (Just dziObjectName) -> do
+        logInfo "Upload DZI manifest"
+          [ "objectName" .= dziObjectName ]
+        _ <- putContent meta path manifestMIME container dziObjectName
+        return ()
+      _ ->
+        logError "Invalid DZI manifest object name"
+          [ "path" .= path
+          , "rootPath" .= rootPath
+          ]
+  where
+    manifestMIME = MIME.Type (MIME.Application "xml") []
+    tileMIME = toMIME (dziTileFormat dzi)
+    container = raxContainer raxConfig
+    raxCreds = mkCredentials (raxUsername raxConfig) (raxApiKey raxConfig)
+
+    stripRoot :: FilePath -> Maybe FilePath
+    stripRoot = stripPrefix (addTrailingPathSeparator rootPath)
+
+    -- TODO: Make `content` prefix configurable:
+    objectNamePrefix = "content"
+
+    toObjectName :: FilePath -> Maybe ObjectName
+    toObjectName p = stripRoot p >>= Just . (objectNamePrefix </>)
+      >>= parseObjectName
 
 getDZITilePaths :: FilePath -> IO [FilePath]
 getDZITilePaths dziPath = do
