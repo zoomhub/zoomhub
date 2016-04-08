abe4b128d88c42ca1b24ef35582ff987cd40f956
Daniel Gasienica <daniel@gasienica.ch>
2016-04-08 01:19:19 -0700

    Log config values at startup

--- src/ZoomHub/Types/ContentBaseURI.hs
+++ src/ZoomHub/Types/ContentBaseURI.hs
@@ -3,9 +3,14 @@ module ZoomHub.Types.ContentBaseURI
   , unContentBaseURI
   ) where
 
+import           Data.Aeson  (ToJSON, Value (String), toJSON)
+import qualified Data.Text   as T
 import           Network.URI (URI)
 
 newtype ContentBaseURI = ContentBaseURI { unContentBaseURI :: URI } deriving Eq
 
 instance Show ContentBaseURI where
   show = show . unContentBaseURI
+
+instance ToJSON ContentBaseURI where
+  toJSON = String . T.pack . show
--- src/ZoomHub/Types/StaticBaseURI.hs
+++ src/ZoomHub/Types/StaticBaseURI.hs
@@ -3,9 +3,14 @@ module ZoomHub.Types.StaticBaseURI
   , unStaticBaseURI
   ) where
 
+import           Data.Aeson  (ToJSON, Value (String), toJSON)
+import qualified Data.Text   as T
 import           Network.URI (URI)
 
 newtype StaticBaseURI = StaticBaseURI { unStaticBaseURI :: URI } deriving Eq
 
 instance Show StaticBaseURI where
   show = show . unStaticBaseURI
+
+instance ToJSON StaticBaseURI where
+  toJSON = String . T.pack . show
