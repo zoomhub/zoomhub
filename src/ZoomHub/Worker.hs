14584e3ad81c7a236e8068d536e8d10af048f3cb
Daniel Gasienica <daniel@gasienica.ch>
2016-04-08 00:40:53 -0700

    Use `time-units`

--- src/ZoomHub/Worker.hs
+++ src/ZoomHub/Worker.hs
@@ -7,6 +7,8 @@ module ZoomHub.Worker
 import           Control.Concurrent     (threadDelay)
 import           Control.Monad          (forever)
 import           Data.Aeson             ((.=))
+import           Data.Time.Units        (Minute, Second,
+                                         TimeUnit, toMicroseconds)
 
 import           ZoomHub.Config         (Config)
 import qualified ZoomHub.Config         as Config
@@ -14,22 +16,24 @@ import           ZoomHub.Log.Logger     (logDebug)
 import           ZoomHub.Pipeline       (process)
 import           ZoomHub.Storage.SQLite (getNextUnprocessed)
 
--- TODO: Figure out why `time-units` library doesn’t work:
-toMicroseconds :: Int -> Int
-toMicroseconds = (* 10^(6 :: Int))
 
-processExistingContentInterval :: Int
+processExistingContentInterval :: Second
 processExistingContentInterval = 3
+
+processExpiredActiveContentInterval :: Minute
+processExpiredActiveContentInterval = 30
 
 processExistingContent :: Config -> IO ()
 processExistingContent config = forever $ do
-  maybeContent <- getNextUnprocessed (Config.dbConnection config)
-  case maybeContent of
-    Just content -> do
-      _ <- process config content
-      return ()
-    _ -> return ()
+    maybeContent <- getNextUnprocessed (Config.dbConnection config)
+    case maybeContent of
+      Just content -> do
+        _ <- process config content
+        return ()
+      _ -> return ()
 
-  logDebug "Wait for next unprocessed content"
-    ["sleepDuration" .= processExistingContentInterval]
-  threadDelay (toMicroseconds processExistingContentInterval)
+    logDebug "Wait for next unprocessed content"
+      [ "sleepDuration" .= show sleepDuration ]
+    threadDelay . fromIntegral $ toMicroseconds sleepDuration
+  where
+    sleepDuration = processExistingContentInterval
