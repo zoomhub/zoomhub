2ab26eb470acacd65b90e4823508667b9f4ef608
Daniel Gasienica <daniel@gasienica.ch>
2016-04-09 18:10:38 -0700

    :art: Format SQL

--- scripts/convert-production-to-development-db.sql
+++ scripts/convert-production-to-development-db.sql
@@ -1,4 +1,36 @@
-DELETE FROM content WHERE id NOT IN (SELECT id FROM content WHERE hashId IN ('4rcn', '100', 'h', '100U', 'hdfm'));
-DELETE FROM flickr WHERE contentId NOT IN (SELECT id FROM content WHERE hashId IN ('4rcn', '100', 'h', '100U', 'hdfm'));
-DELETE FROM image WHERE contentId NOT IN (SELECT id FROM content WHERE hashId IN ('4rcn', '100', 'h', '100U', 'hdfm'));
-VACUUM;
+DELETE
+FROM content
+WHERE id NOT IN
+    (SELECT id
+     FROM content
+     WHERE hashId IN ('4rcn',
+                      '100',
+                      'h',
+                      '100U',
+                      'hdfm'));
+
+
+DELETE
+FROM flickr
+WHERE contentId NOT IN
+    (SELECT id
+     FROM content
+     WHERE hashId IN ('4rcn',
+                      '100',
+                      'h',
+                      '100U',
+                      'hdfm'));
+
+
+DELETE
+FROM image
+WHERE contentId NOT IN
+    (SELECT id
+     FROM content
+     WHERE hashId IN ('4rcn',
+                      '100',
+                      'h',
+                      '100U',
+                      'hdfm'));
+
+ VACUUM;
