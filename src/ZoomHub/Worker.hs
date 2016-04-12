0065ba6c55a35254f63ec9cff97810af46b0814e
Daniel Gasienica <daniel@gasienica.ch>
2016-04-11 22:13:02 -0700

    Make `dequeueNextUnprocessed` atomic using a transaction

--- src/ZoomHub/Worker.hs
+++ src/ZoomHub/Worker.hs
@@ -5,57 +5,82 @@ module ZoomHub.Worker
   , processExpiredActiveContent
   ) where
 
+import           Control.Exception.Enclosed               (catchAny)
 import           Control.Concurrent        (threadDelay)
 import           Control.Monad             (forever)
 import           Data.Aeson                ((.=))
 import           Data.Time.Units           (Minute, Second, toMicroseconds)
 import           Data.Time.Units.Instances ()
+import qualified Data.Text as T
 
 import           ZoomHub.Config            (Config)
 import qualified ZoomHub.Config            as Config
-import           ZoomHub.Log.Logger        (logDebug, logDebugT)
+import           ZoomHub.Log.Logger        (logInfoT, logInfo)
 import           ZoomHub.Pipeline          (process)
 import           ZoomHub.Storage.SQLite    (getExpiredActive,
-                                            getNextUnprocessed,
-                                            resetAsInitialized, withConnection)
+                                            dequeueNextUnprocessed,
+                                            resetAsInitialized,
+                                            withConnection,
+                                            markAsSuccess,
+                                            markAsFailure)
 import           ZoomHub.Types.Content     (contentId)
 
-
+-- Constants
 processExistingContentInterval :: Second
 processExistingContentInterval = 3
 
 processExpiredActiveContentInterval :: Minute
 processExpiredActiveContentInterval = 30
 
+-- Public API
 processExistingContent :: Config -> String -> IO ()
 processExistingContent config workerId = forever $
-  withConnection (Config.dbPath config) $ \dbConn -> do
-    logDebug "Get next unprocessed content"
-      [ "worker" .= workerId ]
-    maybeContent <- getNextUnprocessed dbConn
+  withConnection dbPath $ \dbConn -> do
+
+    maybeContent <- logT "Get next unprocessed content and mark as active" [] $
+      dequeueNextUnprocessed dbConn
+
     case maybeContent of
       Just content -> do
-        _ <- process config content
-        return ()
-      _ -> return ()
+        logT "Process content: success"
+          [ "id" .= contentId content ] $ do
+            processed <- process raxConfig tempPath content `catchAny` \e -> do
+              let errorMessage = Just . T.pack $ show e
+              logT "Process content: failure"
+                [ "id" .= contentId content
+                , "error" .= errorMessage
+                ] $ markAsFailure dbConn content errorMessage
 
-    logDebug "Wait for next unprocessed content"
-      [ "sleepDuration" .= sleepDuration
-      , "worker" .= workerId
-      ]
+            _ <- markAsSuccess dbConn processed
+            return ()
+      Nothing -> return ()
+
+    logInfo "Wait for next unprocessed content" $
+      [ "sleepDuration" .= sleepDuration ] ++ extraLogMeta
+
     threadDelay . fromIntegral $ toMicroseconds sleepDuration
   where
+    dbPath = Config.dbPath config
+    raxConfig = Config.rackspace config
+    tempPath = Config.tempPath config
+
     sleepDuration = processExistingContentInterval
+
+    logT msg meta action = logInfoT msg (meta ++ extraLogMeta) action
+    extraLogMeta =
+      [ "worker" .= workerId
+      , "topic" .= ("worker:process:existing" :: T.Text)
+      ]
 
 processExpiredActiveContent :: Config -> IO ()
 processExpiredActiveContent config = forever $
   withConnection (Config.dbPath config) $ \dbConn -> do
     cs <- getExpiredActive dbConn
-    logDebugT "Reset expired active content"
+    logInfoT "Reset expired active content"
       [ "ids" .= map contentId cs ]
       (resetAsInitialized dbConn cs)
 
-    logDebug "Wait for next expired active content"
+    logInfo "Wait for next expired active content"
       [ "sleepDuration" .= sleepDuration ]
     threadDelay . fromIntegral $ toMicroseconds sleepDuration
   where
