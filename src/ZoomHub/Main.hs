9cc0b47bfb486d039253889d3c016b80ed39cf4f
Daniel Gasienica <daniel@gasienica.ch>
2016-04-12 00:26:46 -0700

    Reduce concurrency

--- src/ZoomHub/Main.hs
+++ src/ZoomHub/Main.hs
@@ -146,7 +146,7 @@ main = do
       -- Workers
       numProcessors <- getNumProcessors
       numCapabilities <- getNumCapabilities
-      let numProcessingWorkers = numCapabilities * 2
+      let numProcessingWorkers = numCapabilities
       logInfo "Config: Worker"
         [ "numProcessors" .= numProcessors
         , "numCapabilities" .= numCapabilities
