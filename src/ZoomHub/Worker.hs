31b2fd83f61867163c1f7b754125b5dcb19a11e1
Daniel Gasienica <daniel@gasienica.ch>
2016-04-11 23:58:17 -0700

    Catch worker errors and jitter sleep

--- src/ZoomHub/Worker.hs
+++ src/ZoomHub/Worker.hs
@@ -5,68 +5,80 @@ module ZoomHub.Worker
   , processExpiredActiveContent
   ) where
 
-import           Control.Exception.Enclosed               (catchAny)
-import           Control.Concurrent        (threadDelay)
-import           Control.Monad             (forever)
-import           Data.Aeson                ((.=))
-import           Data.Time.Units           (Minute, Second, toMicroseconds)
-import           Data.Time.Units.Instances ()
-import qualified Data.Text as T
+import           Control.Concurrent         (threadDelay)
+import           Control.Exception.Enclosed (catchAny)
+import           Control.Monad              (forever)
+import           Data.Aeson                 ((.=))
+import qualified Data.Text                  as T
+import           Data.Time.Units            (Minute, Second, fromMicroseconds,
+                                             toMicroseconds)
+import           Data.Time.Units.Instances  ()
+import           System.Random              (randomRIO)
 
-import           ZoomHub.Config            (Config)
-import qualified ZoomHub.Config            as Config
-import           ZoomHub.Log.Logger        (logInfoT, logInfo)
-import           ZoomHub.Pipeline          (process)
-import           ZoomHub.Storage.SQLite    (getExpiredActive,
-                                            dequeueNextUnprocessed,
-                                            resetAsInitialized,
-                                            withConnection,
-                                            markAsSuccess,
-                                            markAsFailure)
-import           ZoomHub.Types.Content     (contentId)
+import           ZoomHub.Config             (Config)
+import qualified ZoomHub.Config             as Config
+import           ZoomHub.Log.Logger         (logException, logInfo, logInfoT)
+import           ZoomHub.Pipeline           (process)
+import           ZoomHub.Storage.SQLite     (dequeueNextUnprocessed,
+                                             getExpiredActive, markAsFailure,
+                                             markAsSuccess, resetAsInitialized,
+                                             withConnection)
+import           ZoomHub.Types.Content      (contentId)
 
 -- Constants
-processExistingContentInterval :: Second
-processExistingContentInterval = 3
+processExistingContentInterval :: Minute
+processExistingContentInterval = 1
 
 processExpiredActiveContentInterval :: Minute
 processExpiredActiveContentInterval = 30
 
 -- Public API
 processExistingContent :: Config -> String -> IO ()
-processExistingContent config workerId = forever $
-  withConnection dbPath $ \dbConn -> do
+processExistingContent config workerId = forever $ do
+    logInfo "worker:start" [ "worker" .= workerId ]
 
-    maybeContent <- logT "Get next unprocessed content and mark as active" [] $
-      dequeueNextUnprocessed dbConn
+    go `catchAny` \ex -> do
+      -- TODO: Mark as `completed:failure` or `initialized`:
+      logException "worker:error" ex extraLogMeta
 
-    case maybeContent of
-      Just content -> do
-        logT "Process content: success"
-          [ "id" .= contentId content ] $ do
-            processed <- process raxConfig tempPath content `catchAny` \e -> do
-              let errorMessage = Just . T.pack $ show e
-              logT "Process content: failure"
-                [ "id" .= contentId content
-                , "error" .= errorMessage
-                ] $ markAsFailure dbConn content errorMessage
+    logInfo "worker:end" extraLogMeta
 
-            _ <- markAsSuccess dbConn processed
-            return ()
-      Nothing -> return ()
+    let delta = (2 * (toMicroseconds sleepBase)) `div` 2
+    jitter <- randomRIO (0, delta)
+    let sleepDuration = fromMicroseconds $ delta + jitter :: Second
 
     logInfo "Wait for next unprocessed content" $
       [ "sleepDuration" .= sleepDuration ] ++ extraLogMeta
-
     threadDelay . fromIntegral $ toMicroseconds sleepDuration
   where
+    go = withConnection dbPath $ \dbConn -> do
+      maybeContent <-
+        logT "Get next unprocessed content and mark as active" [] $
+          dequeueNextUnprocessed dbConn
+
+      case maybeContent of
+        Just content ->
+          logT "Process content: success"
+            [ "id" .= contentId content ] $ do
+              processed <-
+                process workerId raxConfig tempPath content `catchAny` \e -> do
+                  let errorMessage = Just . T.pack $ show e
+                  logT "Process content: failure"
+                    [ "id" .= contentId content
+                    , "error" .= errorMessage
+                    ] $ markAsFailure dbConn content errorMessage
+
+              _ <- markAsSuccess dbConn processed
+              return ()
+        Nothing -> return ()
+
     dbPath = Config.dbPath config
     raxConfig = Config.rackspace config
     tempPath = Config.tempPath config
 
-    sleepDuration = processExistingContentInterval
+    sleepBase = processExistingContentInterval
 
-    logT msg meta action = logInfoT msg (meta ++ extraLogMeta) action
+    logT msg meta = logInfoT msg (meta ++ extraLogMeta)
     extraLogMeta =
       [ "worker" .= workerId
       , "topic" .= ("worker:process:existing" :: T.Text)
