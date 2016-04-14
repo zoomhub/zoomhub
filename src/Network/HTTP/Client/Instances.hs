98689fe0541c3b31a1a09423fd7cc103c884b5fe
Daniel Gasienica <daniel@gasienica.ch>
2016-04-13 23:57:05 -0700

    Add `ToJSON` instance for `HttpException`

---
+++ src/Network/HTTP/Client/Instances.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Network.HTTP.Client.Instances where
+
+import           Data.Aeson                (ToJSON, Value (String), object,
+                                            toJSON, (.=))
+import           Data.CaseInsensitive      (original)
+import qualified Data.HashMap.Strict       as HM
+import           Data.Text                 (Text)
+import qualified Data.Text                 as T
+import           Data.Text.Encoding        (decodeUtf8)
+import           Network.HTTP.Client       (HttpException (StatusCodeException))
+import           Network.HTTP.Types        (Header, ResponseHeaders)
+import           Network.HTTP.Types.Status (statusCode)
+
+instance ToJSON HttpException where
+  toJSON (StatusCodeException status headers _) = object
+    [ "type" .= ("StatusCodeException" :: Text)
+    , "status" .= statusCode status
+    , "headers" .= headersToJSON headers
+    ]
+  toJSON e = String . T.pack . show $ e
+
+-- Duplicated from `RequestLogger`:
+toObject :: ToJSON a => [(Text, a)] -> Value
+toObject = toJSON . HM.fromList
+
+headersToJSON :: ResponseHeaders -> Value
+headersToJSON = toObject . map hToJ
+  where
+    headerToJSON' ("Cookie", _) = ("Cookie" :: Text, "<redacted>" :: Text)
+    headerToJSON' ("X-Response-Body-Start", v) =
+      ("X-Response-Body-Start" :: Text, decodeUtf8 $ take 256 v)
+    headerToJSON' hd = headerToJSON hd
+
+    headerToJSON :: Header -> (Text, Text)
+    headerToJSON (headerName, header) =
+      (decodeUtf8 . original $ headerName, decodeUtf8 header)
