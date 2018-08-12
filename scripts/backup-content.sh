9a091a03edf170eaca3b67611f6c6b1a87f822c1
Daniel Gasienica <daniel@gasienica.ch>
2018-08-11 23:07:53 -0400

    Make consistent backups using SQLite `backup` command

--- scripts/backup-content.sh
+++ scripts/backup-content.sh
@@ -4,9 +4,14 @@
 # Make a resumable local backup of our content using `rsync`:
 #
 
+TIMESTAMP=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
+echo "Timestamp: $TIMESTAMP"
+
 mkdir -p ~/zoomhub-backup
+ssh -t admin@zoomhub.net \
+  "cd /mnt/datavolume/zoomhub/data/; sqlite3 zoomhub.sqlite3 \".backup 'zoomhub-backup-$TIMESTAMP.sqlite3'\""
 rsync -avz \
   -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
   --progress \
-  admin@zoomhub.net:/mnt/datavolume/zoomhub/data/zoomhub.sqlite3 \
-  ~/zoomhub-backup/zoomhub-production-$(date -u +"%Y-%m-%dT%H-%M-%SZ").sqlite3
+  admin@zoomhub.net:"/mnt/datavolume/zoomhub/data/zoomhub-backup-$TIMESTAMP.sqlite3" \
+  ~/zoomhub-backup/zoomhub-production-$TIMESTAMP.sqlite3
