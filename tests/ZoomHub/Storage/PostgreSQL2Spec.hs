5daa62a8aad67559ecdbc134494a0eae5c7cb353
Daniel Gasienica <daniel@gasienica.ch>
2019-03-31 16:54:35 -0400

    Test `initialize`

--- tests/ZoomHub/Storage/PostgreSQL2Spec.hs
+++ tests/ZoomHub/Storage/PostgreSQL2Spec.hs
@@ -13,11 +13,37 @@ import qualified Generics.SOP as SOP
 import Squeal.PostgreSQL (Connection, connectdb, finish, runPQ)
 import Squeal.PostgreSQL.Migration (migrateDown, migrateUp)
 import Test.Hspec
-  (Spec, around, describe, expectationFailure, hspec, it, shouldBe)
+  ( Spec
+  , around
+  , describe
+  , expectationFailure
+  , hspec
+  , it
+  , shouldBe
+  , shouldSatisfy
+  )
 
+import Data.Time.Clock (NominalDiffTime, UTCTime, addUTCTime, getCurrentTime)
 import ZoomHub.Storage.PostgreSQL2 (getById, getById', initialize)
 import ZoomHub.Storage.PostgreSQL2.Schema (Schema, migrations)
-import ZoomHub.Types.Content (contentId, contentNumViews)
+import ZoomHub.Types.Content
+  ( contentActiveAt
+  , contentCompletedAt
+  , contentDZI
+  , contentError
+  , contentId
+  , contentInitializedAt
+  , contentMIME
+  , contentNumViews
+  , contentProgress
+  , contentSize
+  , contentState
+  , contentType
+  , contentURL
+  )
+import qualified ZoomHub.Types.ContentId as ContentId
+import ZoomHub.Types.ContentState (ContentState(Initialized))
+import ZoomHub.Types.ContentType (ContentType(Unknown))
 import ZoomHub.Types.ContentURI (ContentURI'(ContentURI))
 
 main :: IO ()
@@ -39,6 +65,30 @@ withDatabaseConnection = bracket acquire release
 spec :: Spec
 spec =
   around withDatabaseConnection $ do
+    describe "initialize" $
+      it "should return initialized content" $ \conn -> do
+        let url = ContentURI "https://example.com/1"
+        currentTime <- getCurrentTime
+        (mContent, _) <- runPQ (initialize url) conn
+        case mContent of
+          Just content -> do
+            contentId content `shouldBe` ContentId.fromString "X75"
+            contentType content `shouldBe` Unknown
+            contentURL content `shouldBe` url
+            contentState content `shouldBe` Initialized
+            contentInitializedAt content `shouldSatisfy`
+              isWithinSecondsOf currentTime 3
+            contentActiveAt content `shouldBe` Nothing
+            contentCompletedAt content `shouldBe` Nothing
+            contentMIME content `shouldBe` Nothing
+            contentSize content `shouldBe` Nothing
+            contentProgress content `shouldBe` 0.0
+            contentNumViews content `shouldBe` 0
+            contentError content `shouldBe` Nothing
+            contentDZI content `shouldBe` Nothing
+          Nothing ->
+            expectationFailure "expected content to be initialized"
+
     describe "getById" $
       it "should return item by hash ID" $ \conn -> do
         (mContent, _) <- runPQ (initialize $ ContentURI "https://example.com/1") conn
@@ -62,3 +112,10 @@ spec =
             result `shouldBe` Just (content{ contentNumViews = 3 })
           Nothing ->
             expectationFailure "expected content to be initialized"
+
+    where
+    isWithinSecondsOf :: UTCTime -> NominalDiffTime -> UTCTime -> Bool
+    isWithinSecondsOf pivot interval t =
+      let upperBound = addUTCTime interval pivot
+          lowerBound = addUTCTime (-interval) pivot
+      in lowerBound <= t && t <= upperBound
