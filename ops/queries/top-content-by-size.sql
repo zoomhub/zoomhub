eeff083152990444e934a60cacaaca2e5fc2597f
Daniel Gasienica <daniel@gasienica.ch>
2019-11-11 00:05:29 -0500

    Migrate report from SQLite to PostgreSQL

--- ops/queries/top-content-by-size.sql
+++ ops/queries/top-content-by-size.sql
@@ -1,12 +1,12 @@
 SELECT
-  ("http://zoomhub.net/" || content.hashId) AS URL,
-  content.numViews AS Views,
+  ('http://zoomhub.net/' || content.hash_id) AS URL,
+  content.num_views AS Views,
   ROUND(image.width * image.height / 1e6, 1) AS Megapixels
 FROM
   content, image
 WHERE
-  content.id = image.contentid AND
+  content.id = image.content_id AND
   content.state = 'completed:success' AND
-  content.numViews >= 200
+  content.num_views >= 200
 ORDER BY (image.width * image.height) DESC
 LIMIT 100;
