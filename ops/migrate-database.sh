8425f56b60110bc0468de7dfacb0e9d049664556
Daniel Gasienica <daniel@gasienica.ch>
2019-12-19 00:55:28 -0500

    HACK: Tweak environment variables

--- ops/migrate-database.sh
+++ ops/migrate-database.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "PGDATABASE: $PGDATABASE"
+echo "PGHOST: $PGHOST"
+echo "PGPORT: $PGPORT"
 echo "PGUSER: $PGUSER"
+echo "PGDATABASE: $PGDATABASE"
 
 PGHOST='localhost' \
 PGPORT='9001' \
+PGUSER='zoomhub_admin' \
   ./migrate-database "$PGDATABASE" status
