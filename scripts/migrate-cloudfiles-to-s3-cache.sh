a59a1abf860bea83cd3007f0b1eeb8ece3d2ebad
Daniel Gasienica <daniel@gasienica.ch>
2020-06-02 00:31:01 +0200

    Add retries

--- scripts/migrate-cloudfiles-to-s3-cache.sh
+++ scripts/migrate-cloudfiles-to-s3-cache.sh
@@ -13,6 +13,9 @@ while IFS='' read -r LINE || [ -n "${LINE}" ]; do
     zoomhub-aws-s3:cache.zoomhub.net/content \
     --progress \
     --fast-list \
+    --low-level-retries 8 \
+    --retries 8 \
+    --retries-sleep=2s \
     --s3-acl='public-read' # --dry-run
   rclone copy \
     zoomhub-rackspace-cloudfiles:cache/content/${LINE}_files \
@@ -21,6 +24,9 @@ while IFS='' read -r LINE || [ -n "${LINE}" ]; do
     --fast-list \
     --transfers=32 \
     --checkers=32 \
+    --low-level-retries 8 \
+    --retries 8 \
+    --retries-sleep=2s \
     --s3-acl='public-read' # --dry-run
 
 done
