5ca3a38465ceb94e204372208a27736fe4299d47
Daniel Gasienica <daniel@gasienica.ch>
2020-06-20 13:54:24 +0200

    Move environment logging

--- ops/aws/run.sh
+++ ops/aws/run.sh
@@ -19,11 +19,11 @@ eb_environment_type='SingleInstance'
 
 is_leader=$(if [[ -f "/tmp/is_leader" ]]; then echo 'true'; else echo 'false'; fi)
 
-echo "{$(json_log_time), \"message\": \"Run startup script\", \"script\": \"run.sh\", \"environmentType\": \"$eb_environment_type\", \"isLeader\": $is_leader}"
+echo "{$(json_log_time), \"message\": \"Run startup script\", \"script\": \"run.sh\", \"environmentType\": \"$eb_environment_type\", \"isLeader\": $is_leader, \"env\": {\"RDS_HOSTNAME\": "$RDS_HOSTNAME", \"RDS_DB_NAME\": "$RDS_DB_NAME", \"RDS_USERNAME\": "$RDS_USERNAME"}}"
 if [[  "$eb_environment_type" == "SingleInstance" || ("$eb_environment_type" == "LoadBalanced" && "$is_leader" == "true" ) ]]; then
   echo "{$(json_log_time), \"message\": \"Migrate database\", \"script\": \"run.sh\"}"
   /opt/zoomhub/migrate-database $PGDATABASE migrate
 fi
 
-echo "{$(json_log_time), \"message\": \"Run app\", \"script\": \"run.sh\", \"env\": {\"RDS_HOSTNAME\": "$RDS_HOSTNAME", \"RDS_DB_NAME\": "$RDS_DB_NAME", \"RDS_USERNAME\": "$RDS_USERNAME"}}"
+echo "{$(json_log_time), \"message\": \"Run app\", \"script\": \"run.sh\"}"
 PGHOST="$RDS_HOSTNAME" PGPORT="$RDS_PORT" PGDATABASE="$RDS_DB_NAME" PGUSER="$RDS_USERNAME" PGPASSWORD="$RDS_PASSWORD" /opt/zoomhub/zoomhub
