e100cf2ca7220a45b35db13d823b950bc8b1dc95
Daniel Gasienica <daniel@gasienica.ch>
2020-07-07 07:17:22 +0200

    Revert "Add DZI check script"
    This reverts commit 916ba63823a0c9e88a754fbba2c0ba9928b3af21.

--- scripts/check-dzi-uploads.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# set -eo pipefail
-
-echo 'SELECT hashId FROM content WHERE state="completed:success" ORDER BY completedAt;' | \
-  sqlite3 --noheader ~/zoomhub-backup/zoomhub-production-2019-11-11T00-41-37Z.sqlite3  | \
-  xargs -I {} curl -s -o /dev/null -w '{} %{http_code}\n' 'http://cache.zoomhub.net/content/{}.dzi' | \
-  grep -v 200
