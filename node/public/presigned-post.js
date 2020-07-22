96dee58160cb199f723181bf365286d9f4a614df
Daniel Gasienica <daniel@gasienica.ch>
2020-07-23 01:45:11 +0200

    POC: Node.js S3 POST policy signing server

---
+++ node/public/presigned-post.js
@@ -0,0 +1,34 @@
+async function uploadViaPresignedPost(api, file) {
+  // Get presigned POST URL and form fields
+  let response = await fetch(api + "signed-post");
+  let json = await response.json();
+
+  // Build a form for the request body
+  let form = new FormData();
+  Object.keys(json.data.fields).forEach((key) =>
+    form.append(key, json.data.fields[key])
+  );
+  form.append("file", file);
+
+  // Send the POST request
+  // response = await fetch(json.data.url, { method: "POST", body: form });
+  const uploadResponse = await fetch(
+    "http://uploads-development.zoomhub.net.s3.amazonaws.com",
+    { method: "POST", body: form }
+  );
+  if (!uploadResponse.ok) {
+    return "Failed to upload via presigned POST";
+  }
+
+  const successMetadata = await uploadResponse.json();
+
+  // Done!
+  return `
+    <p>File uploaded via presigned POST with key: ${json.id}</p>
+    <p>JSON response: <code>${JSON.stringify(
+      successMetadata,
+      null,
+      2
+    )}</code></p>
+  `;
+}
