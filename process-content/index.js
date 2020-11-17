87e77866be8882c3ad9bbe551b2ba8ddcfd10ba2
Daniel Gasienica <daniel@gasienica.ch>
2020-11-16 23:08:55 -0600

    Implement processing generic URL

--- process-content/index.js
+++ process-content/index.js
@@ -1,34 +1,70 @@
+const AmazonS3URI = require("amazon-s3-uri")
 const AWS = require("aws-sdk")
+const axios = require("axios")
+const FileType = require("file-type")
 const fs = require("fs").promises
 const sharp = require("sharp")
-
-const s3 = new AWS.S3({ apiVersion: "2006-03-01" })
+const uuid = require("uuid")
 
 exports.handler = async (event, context) => {
-  if (!event.bucket) {
-    return "Please specify a URL to process"
-  }
-  if (!event.key) {
+  if (!event.url) {
     return "Please specify a URL to process"
   }
 
-  const result = await s3
-    .getObject({ Bucket: event.bucket, Key: event.key })
-    .promise()
+  const url = event.url
+  console.log("url:", url)
 
-  await fs.writeFile("/tmp/download.jpg", result.Body)
-  await sharp("/tmp/download.jpg")
-    .jpeg()
+  const s3URL = parseS3URL(url)
+  console.log("s3URL:", s3URL)
+
+  const body = await (s3URL ? fetchS3URL(s3URL) : fetchGenericURL({ url }))
+
+  console.log("typeof body", typeof body)
+
+  const outputPath = `/tmp/${uuid.v4()}`
+  console.log("outputPath:", outputPath)
+
+  await fs.writeFile(outputPath, body)
+
+  const fileType = await FileType.fromFile(outputPath)
+  console.log("fileType:", fileType)
+
+  const isPNG = fileType && fileType.mime === "image/png"
+  const tileFormat = isPNG ? { id: "png" } : { id: "jpg", quality: 90 }
+
+  console.log("tileFormat:", tileFormat)
+
+  await sharp(outputPath)
+    .toFormat(tileFormat)
     .tile({
       depth: "onepixel",
       layout: "dz",
       overlap: 1,
       size: 254,
     })
-    .toFile("/tmp/output.dz")
-  const dziXML = await fs.readFile("/tmp/output.dzi", "utf8")
+    .toFile(`${outputPath}.dz`)
+  const dziXML = await fs.readFile(`${outputPath}.dzi`, "utf8")
 
   // output.dzi is the Deep Zoom XML definition
   // output_files contains 512x512 tiles grouped by zoom level
   console.log(dziXML)
+}
+
+const parseS3URL = (url) => {
+  try {
+    return AmazonS3URI(url)
+  } catch (error) {
+    return null
+  }
+}
+
+const fetchS3URL = async ({ bucket, key }) => {
+  const s3 = new AWS.S3({ apiVersion: "2006-03-01" })
+  const result = await s3.getObject({ Bucket: bucket, Key: key }).promise()
+  return result.Body
+}
+
+const fetchGenericURL = async ({ url }) => {
+  const response = await axios.get(url, { responseType: "arraybuffer" })
+  return response.data
 }
