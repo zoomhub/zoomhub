a2beb8dd8a48ab5e49e4d51d12a96c660b67998d
Daniel Gasienica <daniel@gasienica.ch>
2020-11-19 23:19:22 -0600

    Upload DZI tiles and manifest in parallel

--- process-content/index.js
+++ process-content/index.js
@@ -2,37 +2,56 @@ const AmazonS3URI = require("amazon-s3-uri")
 const AWS = require("aws-sdk")
 const axios = require("axios")
 const FileType = require("file-type")
-const fs = require("fs").promises
+const fs = require("fs")
+const path = require("path")
+const PQueue = require("p-queue").default
+const readdir = require("recursive-readdir")
+const rmfr = require("rmfr")
 const sharp = require("sharp")
-const uuid = require("uuid")
+
+const ROOT_PATH = "/tmp"
+const TILE_FORMAT = {
+  jpg: "image/jpeg",
+  png: "image/png",
+}
+
+const s3Client = new AWS.S3({ apiVersion: "2006-03-01" })
 
 exports.handler = async (event, context) => {
+  if (!event.contentId) {
+    return "Please specify a contentID to process"
+  }
   if (!event.url) {
     return "Please specify a URL to process"
   }
 
+  const contentId = event.contentId
   const url = event.url
-  console.log("url:", url)
-
   const s3URL = parseS3URL(url)
-  console.log("s3URL:", s3URL)
 
-  const body = await (s3URL ? fetchS3URL(s3URL) : fetchGenericURL({ url }))
+  console.log("meta", { contentId, url, s3URL })
 
-  console.log("typeof body", typeof body)
+  const body = await (s3URL
+    ? fetchS3URL({ ...s3URL, s3Client })
+    : fetchGenericURL({ url }))
 
-  const outputPath = `/tmp/${uuid.v4()}`
+  const outputPath = `${ROOT_PATH}/${contentId}`
   console.log("outputPath:", outputPath)
 
-  await fs.writeFile(outputPath, body)
+  // Clean up output directory
+  await Promise.all([
+    rmfr(outputPath),
+    rmfr(`${outputPath}_files`),
+    rmfr(`${outputPath}.dzi`),
+  ])
+
+  // Write source file
+  await fs.promises.writeFile(outputPath, body)
 
   const fileType = await FileType.fromFile(outputPath)
-  console.log("fileType:", fileType)
-
   const isPNG = fileType && fileType.mime === "image/png"
   const tileFormat = isPNG ? { id: "png" } : { id: "jpg", quality: 90 }
-
-  console.log("tileFormat:", tileFormat)
+  console.log("file meta:", { fileType, isPNG, tileFormat })
 
   await sharp(outputPath)
     .toFormat(tileFormat)
@@ -43,28 +62,62 @@ exports.handler = async (event, context) => {
       size: 254,
     })
     .toFile(`${outputPath}.dz`)
-  const dziXML = await fs.readFile(`${outputPath}.dzi`, "utf8")
 
-  // output.dzi is the Deep Zoom XML definition
-  // output_files contains 512x512 tiles grouped by zoom level
-  console.log(dziXML)
+  await uploadDZI({ s3Client, basePath: outputPath, tileFormat })
 }
 
 const parseS3URL = (url) => {
   try {
     return AmazonS3URI(url)
   } catch (error) {
+    // ignore errors
     return null
   }
 }
 
-const fetchS3URL = async ({ bucket, key }) => {
-  const s3 = new AWS.S3({ apiVersion: "2006-03-01" })
-  const result = await s3.getObject({ Bucket: bucket, Key: key }).promise()
-  return result.Body
+const fetchS3URL = async ({ s3Client, bucket, key }) =>
+  (await s3Client.getObject({ Bucket: bucket, Key: key }).promise()).Body
+
+const fetchGenericURL = async ({ url }) =>
+  (await axios.get(url, { responseType: "arraybuffer" })).data
+
+const uploadDZI = async ({ s3Client, basePath, tileFormat }) => {
+  const queue = new PQueue({ concurrency: 25 })
+
+  // tiles
+  const tileFileNames = await readdir(`${basePath}_files`)
+  tileFileNames.forEach((fileName) =>
+    queue.add(() =>
+      uploadFile({
+        s3Client,
+        fileName,
+        key: path.relative(ROOT_PATH, fileName),
+        contentType: TILE_FORMAT[tileFormat.id],
+      })
+    )
+  )
+
+  // manifest
+  const dziFileName = `${basePath}.dzi`
+  queue.add(() =>
+    uploadFile({
+      s3Client,
+      fileName: dziFileName,
+      key: path.relative(ROOT_PATH, dziFileName),
+      contentType: "application/xml",
+    })
+  )
 }
 
-const fetchGenericURL = async ({ url }) => {
-  const response = await axios.get(url, { responseType: "arraybuffer" })
-  return response.data
+const uploadFile = async ({ s3Client, fileName, key, contentType }) => {
+  await s3Client
+    .upload({
+      ACL: "public-read",
+      Body: fs.createReadStream(fileName),
+      Bucket: process.env["S3_CACHE_BUCKET"],
+      ContentType: contentType,
+      Key: `content/${key}`,
+    })
+    .promise()
+  console.log("File uploaded:", { fileName, key, contentType })
 }
