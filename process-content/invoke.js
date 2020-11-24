4d208f7a55b7670ab135d93099ea1cf9934f54c7
Daniel Gasienica <daniel@gasienica.ch>
2020-11-23 23:45:13 -0600

    Add `make invoke`

---
+++ process-content/invoke.js
@@ -0,0 +1,7 @@
+const processContent = require("./index").handler
+
+processContent({
+  contentId: "foobar",
+  url:
+    "https://media.vanityfair.com/photos/5866e55b6be3895a2107ebfb/5:3/w_1440,h_864,c_limit/natalie-portman.jpg",
+})
