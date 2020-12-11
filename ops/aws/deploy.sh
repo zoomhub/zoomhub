3118c2af40dea90702ee28d785a5a5ea70dcc431
Daniel Gasienica <daniel@gasienica.ch>
2020-12-11 00:35:30 -0600

    Improve line lengths

--- ops/aws/deploy.sh
+++ ops/aws/deploy.sh
@@ -22,12 +22,18 @@ EB_BUCKET=elasticbeanstalk-$ZH_AWS_EB_PROJECT-deploy-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 
 sed "s/<TAG>/$SHA1/ ; s/<ZH_AWS_ACCOUNT_ID>/$ZH_AWS_ACCOUNT_ID/ ; s/<ZH_AWS_ECR_REPO>/$ZH_AWS_ECR_REPO/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region us-east-2
-aws elasticbeanstalk create-application-version --application-name $ZH_AWS_EB_PROJECT \
-    --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
+aws s3 cp \
+  $DOCKERRUN_FILE \
+  s3://$EB_BUCKET/$DOCKERRUN_FILE \
+  --region us-east-2
+aws elasticbeanstalk create-application-version \
+    --application-name $ZH_AWS_EB_PROJECT \
+    --version-label $SHA1 \
+    --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
     --region us-east-2
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name $ZH_AWS_EB_ENVIRONMENT \
+aws elasticbeanstalk update-environment \
+    --environment-name $ZH_AWS_EB_ENVIRONMENT \
     --version-label $SHA1 \
     --region us-east-2
