ccfa0c9a13b3cd34170273bee2bbd301754690a8
Daniel Gasienica <daniel@gasienica.ch>
2021-09-02 01:27:42 +0200

    Update certbot script
    Version: https://github.com/HausCloud/AWS-EB-SSL/blob/5b962bdc6ff0e2d0dc240424c4f93682b4557770/.platform/hooks/postdeploy/00_ssl_setup_nginx.sh

--- ops/aws/.platform/hooks/postdeploy/00_ssl_setup_certbot.sh
+++ ops/aws/.platform/hooks/postdeploy/00_ssl_setup_certbot.sh
@@ -22,19 +22,24 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# IMPORTANT: use this bash otherwise following error: "failed with error exit status 127. Stderr:/usr/bin/env: bash: No such file or directory"
+# IMPORTANT: use LF instead of CRLF for .sh files, otherwise following error: "00_ssl_setup_certbot.sh: no such file or directory"
+# IMPORTANT: for LF: also set "* text eol=lf" in ".gitattributes" file otherwise git will convert it into CRLF again on Windows :(
+
 set -euo pipefail
 
 # Postdeploy script for enabling SSL (single instance)
 # Compatible only with Amazon Linux 2 EC2 instances
 
+LOG_PATH=$(find /var/log/ -type f -iname 'eb-hooks.log')
+DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+# IMPORTANT: no whitespaces in CERTBOT_NAME, otherwise following error: "invalid number of arguments in "ssl_certificate" directive in /etc/nginx/nginx.conf:81"
 CERTBOT_NAME="$CERTBOT_NAME"
 CERTBOT_EMAIL="$CERTBOT_EMAIL"
 # Multiple domain example: CERTBOT_DOMAINS='bort.com,www.bort.com,bort-env.eba-2kg3gsq2.us-east-2.elasticbeanstalk.com'
 CERTBOT_DOMAINS="$CERTBOT_DOMAINS"
 
-# ---
-
-LOG_PATH=$(find /var/log/ -type f -iname 'eb-hooks.log')
 log_level() {
     if [ -n "$LOG_PATH" ]; then
         DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
@@ -53,24 +58,17 @@ if [ -z "$CERTBOT_NAME" ] || [ -z "$CERTBOT_EMAIL" ] || [ -z "$CERTBOT_DOMAINS" 
     exit 1
 fi
 
-# Auto allow yes for all yum install
-# SUGGESTION: Remove after deployment
-log_debug "yum: assumeyes=1"
-if ! grep -q 'assumeyes=1' /etc/yum.conf; then
-    echo 'assumeyes=1' | tee -a /etc/yum.conf
-fi
-
 # Install EPEL
 # Source: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/amazon-linux-ami-basics.html
 log_debug "yum: Install EPEL"
 if ! yum list installed epel-release; then
-    yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 fi
 
 # Install certbot
 log_debug "yum: Install certbot"
 if yum list installed epel-release && ! command -v certbot &>/dev/null; then
-    yum install certbot python2-certbot-nginx
+    yum install -y certbot python2-certbot-nginx
 fi
 
 
@@ -103,7 +101,7 @@ if command -v certbot &>/dev/null; then
           --keep-until-expiring \
           --non-interactive
     else
-        log_error 'nginx configuration is invalid.'
+        log_error 'Nginx configuration is invalid.'
         exit 1
     fi
 else
