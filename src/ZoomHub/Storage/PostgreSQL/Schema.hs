7d2f344f152ca7debf159b9a6ead94aa7de9ed0e
Daniel Gasienica <daniel@gasienica.ch>
2021-09-28 01:44:37 +0200

    Improve `getNextUnprocessed` query performance
    Add indexes to speed up the slowest query, i.e. `getNextUnprocessed`:
    
    ```sql
    SELECT
        "content"."hash_id" AS "cirHashId",
        "content"."type_id" AS "cirTypeId",
        "content"."url" AS "cirURL",
        "content"."state" AS "cirState",
        "content"."initialized_at" AS "cirInitializedAt",
        "content"."active_at" AS "cirActiveAt",
        "content"."completed_at" AS "cirCompletedAt",
        "content"."title" AS "cirTitle",
        "content"."attribution_text" AS "cirAttributionText",
        "content"."attribution_link" AS "cirAttributionLink",
        "content"."mime" AS "cirMIME",
        "content"."size" AS "cirSize",
        "content"."error" AS "cirError",
        "content"."progress" AS "cirProgress",
        "content"."abuse_level_id" AS "cirAbuseLevelId",
        "content"."num_abuse_reports" AS "cirNumAbuseReports",
        "content"."num_views" AS "cirNumViews",
        "content"."version" AS "cirVersion",
        "content"."submitter_email" AS "cirSubmitterEmail",
        "content"."verification_token" AS "cirVerificationToken",
        "content"."verified_at" AS "cirVerifiedAt",
        "image"."width" AS "cirWidth",
        "image"."height" AS "cirHeight",
        "image"."tile_size" AS "cirTileSize",
        "image"."tile_overlap" AS "cirTileOverlap",
        "image"."tile_format" AS "cirTileFormat"
    FROM
        "content" AS "content"
        LEFT OUTER JOIN "image" AS "image" ON ("content"."id" = "image"."content_id")
    WHERE (("content"."state" = ($1::text))
        AND (("content"."version" >= 5)
            AND "content"."verified_at" IS NOT NULL))
    ORDER BY
        "content"."initialized_at" ASC,
        "content"."num_views" DESC
    LIMIT 1;
    ```

--- src/ZoomHub/Storage/PostgreSQL/Schema.hs
+++ src/ZoomHub/Storage/PostgreSQL/Schema.hs
@@ -20,10 +20,11 @@ import Squeal.PostgreSQL.Migration (Migration (..))
 import qualified ZoomHub.Storage.PostgreSQL.Schema.Schema0 as Schema0
 import qualified ZoomHub.Storage.PostgreSQL.Schema.Schema1 as Schema1
 import qualified ZoomHub.Storage.PostgreSQL.Schema.Schema2 as Schema2
-import ZoomHub.Storage.PostgreSQL.Schema.Schema3 (Schema3)
 import qualified ZoomHub.Storage.PostgreSQL.Schema.Schema3 as Schema3
+import ZoomHub.Storage.PostgreSQL.Schema.Schema4 (Schema4)
+import qualified ZoomHub.Storage.PostgreSQL.Schema.Schema4 as Schema4
 
-type Schemas = Public Schema3
+type Schemas = Public Schema4
 
 migrations :: String -> AlignedList (Migration Definition) (Public '[]) Schemas
 migrations hashidsSecret =
@@ -31,4 +32,5 @@ migrations hashidsSecret =
     >>> Schema1.migration
     :>> Schema2.migration
     :>> Schema3.migration
+    :>> Schema4.migration
     :>> Done
