a272f70b6d983b26e8ded8f96706a94be279bae6
Daniel Gasienica <daniel@gasienica.ch>
2021-10-19 13:06:45 +0200

    Extract classes

--- frontend/src/components/Create.tsx
+++ frontend/src/components/Create.tsx
@@ -8,65 +8,20 @@ import {
 } from "@heroicons/react/solid"
 
 export const Create = () => (
-  <div className="rounded-lg bg-gray-800 bg-opacity-50 p-8 mt-4 shadow-md backdrop-filter backdrop-blur-md">
+  <div className="rounded-lg bg-gray-800 bg-opacity-50 p-8 mt-4 shadow-xl backdrop-filter backdrop-blur-md">
     <form className="grid gap-7">
       <h2 className="text-gray-200 text-center text-3xl font-semibold tracking-tighter">
         Make your images zoomable
       </h2>
-      <div className="inline-grid w-full grid-cols-2 gap-2">
-        <button
-          type="button"
-          className="
-            font-semibold
-            inline-flex
-            items-center
-            justify-center
-            px-4
-            py-3
-            uppercase
-            bg-gray-400
-            hover:bg-gray-600
-            text-sm
-            rounded-lg
-            shadow-lg
-            cursor-pointer
-            text-gray-800
-            hover:text-white
-
-            outline-none
-            focus:ring-4
-            focus:ring-gray-300
-            focus:ring-opacity-90
-          "
-        >
+      <div className="flex w-full items-center">
+        <button type="button" className="flex-grow btn-secondary">
           <LinkIcon className="w-5 h-5 mr-1" />
           Post image link
         </button>
-
-        <label
-          className="
-            font-semibold
-            inline-flex
-            items-center
-            justify-center
-            px-4
-            py-3
-            uppercase
-            bg-gray-400
-            hover:bg-gray-600
-            text-sm
-            rounded-lg
-            shadow-lg
-            cursor-pointer
-            text-gray-800
-            hover:text-white
-
-            outline-none
-            focus:ring-4
-            focus:ring-gray-300
-            focus:ring-opacity-90
-          "
-        >
+        <span className="text-gray-600 mx-2 text-sm font-semibold uppercase">
+          or
+        </span>
+        <label className="flex-grow btn-secondary">
           <UploadIcon className="h-5 w-5 mr-1" />
           Upload image
           <input type="file" className="hidden" />
@@ -76,63 +31,22 @@ export const Create = () => (
       <div className="grid gap-1">
         <label className="text-gray-50 inline-flex items-center text-sm">
           <MailIcon className="h-5 w-5 mr-1" />
-          <strong>Email</strong>{" "}
-          <span className="ml-1">
-            (so we know how to reach you when your image is ready<sup>★</sup>)
-          </span>
+          Email where we can notify you when your image is ready<sup>★</sup>
         </label>
         <input
           type="text"
-          className="
-            w-full
-            px-4
-            py-3
-            rounded
-            placeholder-gray-400
-            text-center
-            outline-none
-            focus:ring-4
-            focus:ring-orange-500
-            focus:ring-opacity-90
-            text-lg
-            font-medium
-          "
+          className="w-full text-input"
           placeholder="you@example.com"
         ></input>
       </div>
 
-      <div>
-        <button
-          type="button"
-          className="
-            w-full
-            font-semibold
-            inline-flex
-            items-center
-            justify-center
-            px-4
-            py-3
-            uppercase
-            bg-orange-500
-            hover:bg-orange-600
-            rounded-lg
-            shadow-lg
-            cursor-pointer
-            text-gray-800
-            hover:text-white
-            text-lg
-
-            outline-none
-            focus:ring-4
-            focus:ring-orange-500
-            focus:ring-opacity-90
-          "
-        >
+      <div className="text-center">
+        <button type="button" className=" w-full btn-primary">
           Create
           <ChevronDoubleRightIcon className="h-5 w-5 ml-1"></ChevronDoubleRightIcon>
         </button>
 
-        <div className="text-gray-500 text-sm mt-4">
+        <div className="text-gray-600 text-sm mt-4">
           <sup>★</sup> We won’t spam you or share your email — promise!
         </div>
       </div>
