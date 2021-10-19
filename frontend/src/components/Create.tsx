3c2edf88bd499d9c136e06238fbec4a00f42f633
Daniel Gasienica <daniel@gasienica.ch>
2021-10-20 00:59:44 +0200

    Implement image URL submission

--- frontend/src/components/Create.tsx
+++ frontend/src/components/Create.tsx
@@ -1,30 +1,65 @@
 import * as React from "react"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
+import axios from "axios"
 import {
   ChevronDoubleRightIcon,
   LinkIcon,
   MailIcon,
+  MailOpenIcon,
 } from "@heroicons/react/solid"
 
 export const Create = () => {
-  const [imageURL, setImageURL] = useState("")
+  const [view, setView] = useState("submit")
 
   return (
-    <div className="grid gap-10 rounded-lg bg-gray-800 bg-opacity-50 py-8 px-6 mt-4 shadow-xl backdrop-filter backdrop-blur-md">
-      <div className="grid place-content-center place-items-center">
-        <h1 className="text-5xl text-white font-semibold tracking-tighter">
-          Instant zoomable images
+    <div
+      className="
+        rounded-lg
+        bg-gray-800/50
+        py-8
+        px-6
+        mt-4
+        shadow-xl
+        backdrop-filter
+        backdrop-blur-md
+        pointer-events-auto
+      "
+    >
+      {(() => {
+        switch (view) {
+          case "submit":
+            return <Submit onViewChange={setView} />
+          case "verify-email":
+            return <VerifyEmail onViewChange={setView} />
+          case "error":
+            return <Error onViewChange={setView} />
+          default:
+            throw new Error(`Invalid view: ${view}`)
+        }
+      })()}
+    </div>
+  )
+}
+
+const Submit = ({ onViewChange }) => {
+  const formRef = useRef(null)
+  const [imageURL, setImageURL] = useState()
+  const [email, setEmail] = useState()
+
+  return (
+    <div className="grid gap-6 lg:gap-10">
+      <div className="grid place-content-center place-items-center text-center">
+        <h1 className="text-4xl lg:text-5xl text-white font-semibold tracking-tighter">
+          Stunning zoomable images
         </h1>
-        <h2 className="text-lg text-gray-400 font-semibold tracking-tighter">
+        <h2 className="text-lg lg:text-xl text-gray-400 font-semibold tracking-tighter">
           Play around with the background: tap, pinch, drag, scroll.
         </h2>
       </div>
       <hr className="border-gray-600/40" />
-      <form className="grid gap-7">
-        <h2 className="text-gray-200 text-center text-3xl font-semibold tracking-tighter">
-          Try it with your own image
-        </h2>
+      <form className="grid gap-7" ref={formRef}>
+        <SectionTitle>Try it with your own image</SectionTitle>
         <div className="w-full">
           <label className="text-gray-50 inline-flex items-center text-sm">
             <LinkIcon className="h-5 w-5 mr-1" />
@@ -48,13 +83,36 @@ export const Create = () => {
             type="email"
             className="w-full text-input"
             placeholder="you@example.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
             required
           />
         </div>
         <div className="text-center">
-          <button type="submit" className=" w-full btn-primary">
+          <button
+            type="submit"
+            className="w-full btn-primary"
+            onClick={async (event) => {
+              event.preventDefault()
+
+              if (!formRef.current.reportValidity()) {
+                return
+              }
+
+              let response
+              try {
+                response = await submitURL({ url: imageURL, email })
+              } catch (error) {
+                console.error("response:", response)
+                onViewChange("error")
+                return
+              }
+
+              onViewChange("verify-email")
+            }}
+          >
             Create
-            <ChevronDoubleRightIcon className="h-5 w-5 ml-1"></ChevronDoubleRightIcon>
+            <ChevronDoubleRightIcon className="h-5 w-5 ml-1" />
           </button>
 
           <div className="text-gray-600 text-sm mt-4">
@@ -65,3 +123,55 @@ export const Create = () => {
     </div>
   )
 }
+
+const VerifyEmail = ({ onViewChange }) => (
+  <div className="text-center">
+    <h1 className="text-4xl text-white tracking-tighter font-semibold">
+      🎉 Success
+    </h1>
+    <div className="text-gray-200 text-2xl tracking-tight font-medium mt-5">
+      📩 Check your email
+    </div>
+    <div className="text-gray-200 text-xl tracking-tight font-medium">
+      We’ve just sent you the link to view your upload.
+    </div>
+
+    <button
+      type="button"
+      className="btn-primary mt-6"
+      onClick={() => onViewChange("submit")}
+    >
+      Create another one
+      <ChevronDoubleRightIcon className="h-5 w-5 ml-1" />
+    </button>
+  </div>
+)
+
+const Error = ({ onViewChange }) => (
+  <div className="text-center">
+    <h1 className="text-4xl text-white tracking-tighter font-semibold">
+      😕 Oops…
+    </h1>
+    <div className="text-gray-200 text-2xl tracking-tight font-medium mt-5">
+      Something went wrong.
+    </div>
+
+    <button
+      type="button"
+      className="btn-primary mt-6"
+      onClick={() => onViewChange("submit")}
+    >
+      Try again
+      <ChevronDoubleRightIcon className="h-5 w-5 ml-1" />
+    </button>
+  </div>
+)
+
+const SectionTitle = ({ children }) => (
+  <h2 className="text-3xl lg:text-4xl text-gray-200 text-center font-semibold tracking-tighter">
+    {children}
+  </h2>
+)
+
+const submitURL = async ({ url, email }) =>
+  await axios.get("/v1/content", { params: { url, email } })
