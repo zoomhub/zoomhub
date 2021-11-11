3d896c52045e1c0b30409a9252d5c9075ed2ab75
Daniel Gasienica <daniel@gasienica.ch>
2021-11-10 22:45:47 -0600

    Add types

--- frontend/src/lib/submitFile.ts
+++ frontend/src/lib/submitFile.ts
@@ -5,14 +5,29 @@ import.meta.hot
 import { PresignedPOSTData } from "../types/PresignedPOSTData"
 import { UploadError } from "./Error"
 
-export const submitFile = async ({ email, file }) => {
-  // TODO: Handle errors
-  const presignedPostData: PresignedPOSTData = await fetch(
-    `${__SNOWPACK_ENV__.SNOWPACK_PUBLIC_API_BASE_URI}/v1/content/upload?email=${email}`
-  ).then((response) => response.json())
+export const submitFile: ({
+  email,
+  file,
+  onProgress,
+}: {
+  email: string
+  file: File
+  onProgress?: (progress: number) => void
+}) => Promise<Response> = async ({ email, file }) => {
+  let presignedPOSTData: PresignedPOSTData
+  try {
+    presignedPOSTData = await fetch(
+      `${__SNOWPACK_ENV__.SNOWPACK_PUBLIC_API_BASE_URI}/v1/content/upload?email=${email}`
+    ).then((response) => response.json())
+  } catch (innerError) {
+    throw new UploadError({
+      message: "Failed to fetch presigned POST data",
+      innerError,
+    })
+  }
 
   const formData = new FormData()
-  Object.entries(presignedPostData)
+  Object.entries(presignedPOSTData)
     .filter(([key, _]) => key !== "url")
     .forEach(([key, value]) => {
       formData.append(key, value)
@@ -21,7 +36,7 @@ export const submitFile = async ({ email, file }) => {
 
   let uploadResponse
   try {
-    uploadResponse = await fetch(presignedPostData.url, {
+    uploadResponse = await fetch(presignedPOSTData.url, {
       method: "POST",
       body: formData,
     })
