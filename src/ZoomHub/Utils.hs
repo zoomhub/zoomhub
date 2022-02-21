4f1b9e5c02e05410de0638b477362c239880e6e2
Daniel Gasienica <daniel@gasienica.ch>
2022-02-21 16:46:03 +0100

    Utils: Add `tshow` function

--- src/ZoomHub/Utils.hs
+++ src/ZoomHub/Utils.hs
@@ -1,12 +1,14 @@
 module ZoomHub.Utils
   ( intercalate,
     lenientDecodeUtf8,
+    tshow,
   )
 where
 
 import Data.ByteString (ByteString)
 import Data.List (intersperse)
 import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8With)
 import Data.Text.Encoding.Error (lenientDecode)
 
@@ -15,3 +17,6 @@ intercalate xs xss = mconcat (intersperse xs xss)
 
 lenientDecodeUtf8 :: ByteString -> Text
 lenientDecodeUtf8 = decodeUtf8With lenientDecode
+
+tshow :: Show a => a -> T.Text
+tshow = T.pack . show
