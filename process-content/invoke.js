17cb8737d673a4cf43ef20da94f4a30cb13d20cb
Daniel Gasienica <daniel@gasienica.ch>
2022-03-31 22:25:36 +0200

    Check for `BASE_URI` + `CONTENT_ID`

--- process-content/invoke.js
+++ process-content/invoke.js
@@ -1,11 +1,17 @@
 const processContent = require("./index").handler
 
+if (!process.env.BASE_URI) {
+  throw new Error("Please set 'BASE_URI' environment variable")
+}
+
+if (!process.env.CONTENT_ID) {
+  throw new Error("Please set 'CONTENT_ID' environment variable")
+}
+
 ;(async () => {
   const result = await processContent(
     {
-      contentURL: `${process.env.BASE_URI}/v1/content/${
-        process.env.CONTENT_ID || "Xar"
-      }`,
+      contentURL: `${process.env.BASE_URI}/v1/content/${process.env.CONTENT_ID}`,
     },
     {}
   )
