731f231421f80d2943496cdfb2bca4c2d0a426e1
Daniel Gasienica <daniel@gasienica.ch>
2024-03-05 00:55:23 +0100

    Add `ContentURI` Squeal / PostgreSQL codecs

--- src/ZoomHub/Types/ContentURI.hs
+++ src/ZoomHub/Types/ContentURI.hs
@@ -12,37 +12,57 @@
 
 module ZoomHub.Types.ContentURI
   ( ContentURI (ContentURI),
+    fromText,
     -- TODO: Can we test this without exporting it?
     unContentURI,
   )
 where
 
+import Control.Monad.Except (MonadError (throwError))
 import Data.Aeson (ToJSON, Value (String), toJSON)
 import Data.Text (Text)
 import qualified Data.Text as T
 import GHC.Generics (Generic)
 import Servant (FromHttpApiData, parseUrlPiece)
-import Squeal.PostgreSQL (FromPG (..), Inline, IsPG, ToPG)
+import Squeal.PostgreSQL (FromPG (..), Inline (inline), IsPG (PG), PGType (PGtext), ToPG (toPG))
 
 newtype ContentURI = ContentURI {unContentURI :: Text}
   deriving stock (Eq, Generic)
-  deriving newtype (IsPG, ToPG db, Inline)
 
 instance Show ContentURI where
-  show = T.unpack . unContentURI
+  show = show . unContentURI
+
+fromText :: Text -> Maybe ContentURI
+fromText t
+  | "http://" `T.isPrefixOf` t = Just $ ContentURI t
+  | "https://" `T.isPrefixOf` t = Just $ ContentURI t
+  | "zoomit://thumbnail/" `T.isPrefixOf` t = Just $ ContentURI t
+  | otherwise = Nothing
 
 -- Text
 instance FromHttpApiData ContentURI where
-  parseUrlPiece t
-    | "http://" `T.isPrefixOf` t = Right $ ContentURI t
-    | "https://" `T.isPrefixOf` t = Right $ ContentURI t
-    | "zoomit://thumbnail/" `T.isPrefixOf` t = Right $ ContentURI t
-    | otherwise = Left "Invalid content URI"
+  parseUrlPiece t = case fromText t of
+    Just cURI -> Right cURI
+    Nothing -> Left "Invalid content URI"
 
 -- JSON
 instance ToJSON ContentURI where
   toJSON = String . unContentURI
 
 -- Squeal / PostgreSQL
+-- TODO: Look into newtype deriving
+instance IsPG ContentURI where
+  type PG ContentURI = 'PGtext
+
+instance ToPG db ContentURI where
+  toPG = toPG . unContentURI
+
 instance FromPG ContentURI where
-  fromPG = ContentURI <$> fromPG @Text
+  fromPG = do
+    value <- fromPG @Text
+    case fromText value of
+      Just format -> pure format
+      Nothing -> throwError $ "Invalid content ID: \"" <> value <> "\""
+
+instance Inline ContentURI where
+  inline = inline . unContentURI
