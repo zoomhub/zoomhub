4b7ddad744267c73c2750c26dd8a6d667449a3b9
Daniel Gasienica <daniel@gasienica.ch>
2024-03-30 00:36:39 +0100

    Implement `uriByteStringToURI`

--- src/ZoomHub/Utils.hs
+++ src/ZoomHub/Utils.hs
@@ -2,15 +2,22 @@ module ZoomHub.Utils
   ( intercalate,
     lenientDecodeUtf8,
     tshow,
+    uriByteStringToURI,
   )
 where
 
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BC
 import Data.List (intersperse)
+import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8With)
 import Data.Text.Encoding.Error (lenientDecode)
+import Flow
+import Network.URI (URI, parseAbsoluteURI)
+import URI.ByteString (serializeURIRef')
+import qualified URI.ByteString as BS
 
 intercalate :: (Monoid a) => a -> [a] -> a
 intercalate xs xss = mconcat (intersperse xs xss)
@@ -20,3 +27,11 @@ lenientDecodeUtf8 = decodeUtf8With lenientDecode
 
 tshow :: (Show a) => a -> T.Text
 tshow = T.pack . show
+
+uriByteStringToURI :: BS.URI -> URI
+uriByteStringToURI uriBS =
+  uriBS
+    |> serializeURIRef'
+    |> BC.unpack
+    |> parseAbsoluteURI
+    |> fromMaybe (error $ "Invalid URI: " <> show uriBS)
