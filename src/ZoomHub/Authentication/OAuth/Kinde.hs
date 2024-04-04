826e7eb6f75f177d250d2ccbc5aa7f1dfefd115c
Daniel Gasienica <daniel@gasienica.ch>
2024-04-04 02:05:16 +0200

    Add basic OAuth 2.0 ‘Authorization Code Flow’

--- src/ZoomHub/Authentication/OAuth/Kinde.hs
+++ src/ZoomHub/Authentication/OAuth/Kinde.hs
@@ -4,20 +4,30 @@
 
 module ZoomHub.Authentication.OAuth.Kinde
   ( mkApp,
+    mkIdp,
+    fetchAccessToken,
   )
 where
 
+import Control.Lens ((.~), (^?))
+import qualified Data.Aeson as JSON
+import Data.Aeson.Lens (key, _String)
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
 import qualified Data.Text.Lazy as L
 import Flow
 import Network.OAuth2.Experiment (AuthorizationCodeApplication (..), AuthorizeState, ClientId (ClientId), ClientSecret (ClientSecret), Idp (..), IdpApplication (IdpApplication))
 import qualified Network.OAuth2.Experiment as OAuth2
-import URI.ByteString (parseURI, strictURIParserOptions)
+import Network.Wreq (FormParam ((:=)), defaults, header, postWith, responseBody)
+import URI.ByteString (parseURI, serializeURIRef', strictURIParserOptions)
+import ZoomHub.Authentication.OAuth (AccessToken (AccessToken), AuthorizationCode (unAuthorizationCode))
 import ZoomHub.Config.Kinde (ClientId (unClientId), ClientSecret (unClientSecret), Domain (unDomain))
 import qualified ZoomHub.Config.Kinde as Kinde
+import ZoomHub.Utils (lenientDecodeUtf8)
 
 mkIdp :: Domain -> Idp "kinde"
 mkIdp domain =
@@ -58,3 +68,25 @@ mkApp config state =
     { OAuth2.idp = mkIdp config.domain,
       OAuth2.application = mkAuthCodeApp config state
     }
+
+fetchAccessToken :: Idp "kinde" -> Kinde.Config -> AuthorizationCode -> IO (Maybe AccessToken)
+fetchAccessToken idp config code = do
+  let opts = defaults |> header "Content-Type" .~ ["application/x-www-form-urlencoded; charset=UTF-8"]
+      tokenUrl = idp |> idpTokenEndpoint |> uriToText |> T.unpack
+      payload =
+        [ "client_id" := (config.clientId |> unClientId),
+          "client_secret" := (config.clientSecret |> unClientSecret),
+          "grant_type" := ("authorization_code" :: Text),
+          "redirect_uri" := (config.redirectURI |> show),
+          "code" := (code |> unAuthorizationCode)
+        ]
+
+  response <- postWith opts tokenUrl payload
+  let accessToken = do
+        body <- response ^? responseBody
+        json <- (body |> JSON.decode) :: Maybe JSON.Value
+        accessTokenStr <- json ^? key "access_token" . _String
+        return $ AccessToken accessTokenStr
+  return accessToken
+  where
+    uriToText uri = uri |> serializeURIRef' |> lenientDecodeUtf8
