a78bee11b15530d4516d39a4c365870f76a3afa4
Daniel Gasienica <daniel@gasienica.ch>
2024-04-05 01:57:34 +0200

    Add `appendQueryParams`

--- src/ZoomHub/Utils.hs
+++ src/ZoomHub/Utils.hs
@@ -2,15 +2,18 @@ module ZoomHub.Utils
   ( intercalate,
     lenientDecodeUtf8,
     tshow,
+    appendQueryParams,
   )
 where
 
+import Control.Lens (over)
 import Data.ByteString (ByteString)
 import Data.List (intersperse)
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8With)
 import Data.Text.Encoding.Error (lenientDecode)
+import URI.ByteString (URIRef, queryL, queryPairsL)
 
 intercalate :: (Monoid a) => a -> [a] -> a
 intercalate xs xss = mconcat (intersperse xs xss)
@@ -20,3 +23,7 @@ lenientDecodeUtf8 = decodeUtf8With lenientDecode
 
 tshow :: (Show a) => a -> T.Text
 tshow = T.pack . show
+
+appendQueryParams :: [(ByteString, ByteString)] -> URIRef a -> URIRef a
+appendQueryParams params =
+  over (queryL . queryPairsL) (params <>)
