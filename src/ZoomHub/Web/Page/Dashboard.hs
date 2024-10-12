7fd9fb34af20179c17a0b188cd16eebe592a071b
Daniel Gasienica <daniel@gasienica.ch>
2024-10-13 00:48:26 +0200

    Show thumbnails on dashboard

--- src/ZoomHub/Web/Page/Dashboard.hs
+++ src/ZoomHub/Web/Page/Dashboard.hs
@@ -14,13 +14,23 @@ import qualified ZoomHub.Web.Page as Page
 import ZoomHub.Authentication.Session (Session)
 import qualified ZoomHub.Authentication.Session as User
 import qualified ZoomHub.Authentication.Session as Session
-import ZoomHub.Types.Content (Content (contentURL))
-import ZoomHub.Types.ContentURI (ContentURI(unContentURI))
+import qualified ZoomHub.Types.Content as Internal
 import Control.Monad (forM_)
+import Data.Foldable (for_)
+import qualified ZoomHub.API.Types.Content as Content
+import qualified Data.Text as T
+import ZoomHub.Types.ContentId (ContentId(unContentId))
+import qualified ZoomHub.API.Types.DeepZoomImage as DZI
+import ZoomHub.Utils (tshow)
+import ZoomHub.API.Types.DeepZoomImage (DeepZoomImage(dziWidth, dziHeight))
+import ZoomHub.Types.BaseURI (BaseURI)
+import ZoomHub.Types.ContentBaseURI (ContentBaseURI)
 
 data Dashboard = Dashboard
   { session :: Session
-  , content :: [Content]
+  , content :: [Internal.Content]
+  , baseURI :: BaseURI
+  , contentBaseURI :: ContentBaseURI
   }
 
 instance H.ToHtml Dashboard where
@@ -34,14 +44,22 @@ instance H.ToHtml Dashboard where
             pageBody =
               H.div_ [H.class_ "p-6"] do
                 H.h1_ [H.class_ "text-3xl text-white font-bold mb-2"] "Dashboard"
-                H.div_ [H.class_ "space-y-10 text-white"] do
+                H.div_ [H.class_ "text-white"] do
                   H.p_ [] $ H.toHtml
                     case currentUser |> User.givenName of
                       Just givenName -> "Hi " <> givenName
                       Nothing -> "Hello"
-                  H.ol_ [] $
-                    forM_ content \c ->
-                      H.li_ [] (H.toHtml (c |> contentURL |> unContentURI))
+                  H.div_ [H.class_ "space-y-4"] $
+                    forM_ content \internalContent ->
+                      let publicContent = Content.fromInternal baseURI contentBaseURI internalContent in
+                      for_ (publicContent |> Content.contentDzi) \dzi ->
+                        H.a_ [H.href_ $ "/" <> (T.pack . unContentId . Internal.contentId $ internalContent)] $
+                          H.img_
+                            [ H.src_ $ tshow $ DZI.largestSingleTileUrl dzi,
+                              H.width_ "256",
+                              H.style_ ("aspect-ratio: " <> tshow (dziWidth dzi) <> " / " <> tshow (dziHeight dzi))
+                            ]
+                      -- H.li_ [] (H.toHtml (c |> contentURL |> unContentURI))
           }
       )
     where
